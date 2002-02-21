e3fbd91c0475ef51bb2d5b5faf90b20052460330
bklock <bklock@5361db82-7b37-4a53-9f17-e17e3a93d091>
2002-02-21 23:49:03 +0000

    addition of infoText and author fields for drivers/devices for use with the "Show Details" dialog

--- JSynthLib/synthdrivers/BossDR660/BossDR660Device.java
+++ JSynthLib/synthdrivers/BossDR660/BossDR660Device.java
@@ -22,6 +22,12 @@ public class BossDR660Device extends Device
         manufacturerName="Boss";
         modelName="DR660";
         synthName="DR660";
+	infoText="JSynthLib functions both as an editor and librarian for DR660 Drumkits. Banks of Drumkits are not "+
+	         "supported due to Roland not documenting the format of the bank sysex dump. Also, while most parameters "+
+		 "are editable, JSynthLib can not currently edit the effects settings of the DR660.\n\n"+
+		 "Keep in mind that the first several locations to store drumkits on the DR660 are ROM"+
+		 " locations and are not user writable. Though JSynthLib can store drumkits to these locations, "+
+		 "they will revert back to their old values once a patch change message is received.";
         addDriver (new BossDR660DrumkitDriver ());
     }
     
