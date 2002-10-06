dbd28fa9c59a31e14d9c387f34c93fbfd78f5fa0
zellyn <zellyn@5361db82-7b37-4a53-9f17-e17e3a93d091>
2002-10-06 22:05:29 +0000

    New constants file intended to be a central place for app constants

---
+++ JSynthLib/core/Constants.java
@@ -0,0 +1,27 @@
+/**
+ * Constants.java - Central place for definition of constants
+ * @author Zellyn Hunter (zellyn@zellyn.com)
+ */
+
+package core;
+
+public class Constants {
+
+	/** Devices Config properties file name */
+	public static String RESOURCE_NAME_DEVICES_CONFIG =
+		"/synthdrivers.properties";
+
+	/** App Config properties file name */
+	public static String FILE_NAME_APP_CONFIG = "JSynthLib.properties";
+	/** App Config file header */
+	public static String APP_CONFIG_HEADER = "JSynthLib Saved Properties";
+
+	/** Driver properties name prefix */
+	public static String PROP_PREFIX_DEVICE_NAME = "deviceName.";
+	/** Device properties class prefix */
+	public static String PROP_PREFIX_DEVICE_CLASS = "deviceClass.";
+
+	/** Number of faders */
+	public static int NUM_FADERS = 33;
+
+}
