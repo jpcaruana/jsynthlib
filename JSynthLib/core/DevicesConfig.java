9f3890e466bca112348b29e31a900524e3a8fc78
zellyn <zellyn@5361db82-7b37-4a53-9f17-e17e3a93d091>
2002-10-06 22:07:11 +0000

    Data side object that provides the app with a list of available device names and classes.  Reads from a properties file.

---
+++ JSynthLib/core/DevicesConfig.java
@@ -0,0 +1,139 @@
+/**
+ * DevicesConfig.java - handles the data-side of figuring out what
+ * devices are available.  Not responsible for display.  Reads the
+ * device config from a properties file, synthdrivers.properties,
+ * expected to be in /synthdrivers.properties in the jar file or
+ * elsewhere on the path.
+ * @author Zellyn Hunter (zellyn@zellyn.com)
+ */
+
+package core;
+
+import java.util.Properties;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.io.InputStream;
+import java.io.IOException;
+
+public class DevicesConfig {
+
+	/* Properties representing config file */
+	private Properties configProps = new Properties();
+	/* Properties representing devices and class names */
+	private Properties deviceProps = new Properties();
+	/* ArrayList of device names */
+	private ArrayList deviceNames = new ArrayList();
+
+	/**
+	 * Main method for debugging - print out all configured Devices
+	 */
+	public static void main(String args[]) {
+		DevicesConfig devConf = new DevicesConfig();
+		devConf.printAll();
+	}
+
+	/**
+	 * Constructor
+	 */
+	public DevicesConfig() {
+
+		// Load properties file
+		InputStream in = this.getClass().getResourceAsStream(Constants.RESOURCE_NAME_DEVICES_CONFIG);
+		//Properties configProps = new Properties();
+		try {
+			configProps.load(in);
+		}
+		catch (IOException e) {
+			ErrorMsg.reportError("Failed Loading Devices",
+								 "Failed loading devices property file. "
+								 + "Resource " + Constants.RESOURCE_NAME_DEVICES_CONFIG
+								 + " cannot be loaded", e);
+		}
+		finally {
+			try {
+				in.close();
+			}
+			catch (IOException e) {
+				// do nothing
+			}
+		}
+
+		//configProps.propertyNames()
+		Enumeration propNames = configProps.propertyNames();
+		while (propNames.hasMoreElements())
+		{
+			String propName = (String)propNames.nextElement();
+			if (propName.startsWith(Constants.PROP_PREFIX_DEVICE_NAME)) {
+				String deviceKey = propName.substring(
+					Constants.PROP_PREFIX_DEVICE_NAME.length());
+				String deviceName = configProps.getProperty(propName);
+				String deviceClass = configProps.getProperty(
+					Constants.PROP_PREFIX_DEVICE_CLASS + deviceKey);
+
+				if ((deviceName==null) || (deviceClass==null)) {
+					ErrorMsg.reportError("Failed loading Devices",
+										 "Config file inconsistency found "
+										 + "for '" + deviceKey + "' device");
+				}
+				else {
+					deviceProps.setProperty(deviceName, deviceClass);
+					this.deviceNames.add(deviceName);
+				}
+			}
+		}
+		Collections.sort(this.deviceNames);
+	}
+
+	/**
+	 * Return the device names
+	 * @return the names of configured devices
+	 */
+	public String[] deviceNames() {
+		String[] retVal = new String[this.deviceNames.size()];
+		return (String[])this.deviceNames.toArray(retVal);
+	}
+
+	/**
+	 * Given a device name, return its class name
+	 * @param deviceName the name of the device
+	 * @return the class name
+	 */
+	public String classNameForDevice(String deviceName) {
+		return this.deviceProps.getProperty(deviceName);
+	}
+
+	/**
+	 * Given a device name, return an instance of its class
+	 * @param deviceName the name of the device
+	 * @return an instance of the device's class
+	 */
+	public Device classForDevice(String deviceName) {
+		Device device = null;
+		String className = this.deviceProps.getProperty(deviceName);
+		try {
+			Class deviceClass = Class.forName(className);
+			device = (Device)deviceClass.newInstance();
+		}
+		catch (Exception e) {
+			ErrorMsg.reportError("Failed to find class for device",
+								 "Failed to find class for device '"
+								 + deviceName + "'");
+		}
+		return device;
+	}
+
+	/**
+	 * Dump out all properties
+	 */
+	public void printAll() {
+		for (Iterator i = this.deviceNames.iterator(); i.hasNext();)
+		{
+			String deviceName = (String)i.next();
+			String deviceClass = this.deviceProps.getProperty(deviceName);
+			System.out.println(deviceName + ":" + deviceClass);
+		}
+	}
+
+}
