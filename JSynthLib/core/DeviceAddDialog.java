2da8a57d1cb92cd6764407762814c86ad464bf54
zellyn <zellyn@5361db82-7b37-4a53-9f17-e17e3a93d091>
2002-10-06 22:14:26 +0000

    Use DevicesConfig to read properties file instead of hardcoding class names and driver descriptions

--- JSynthLib/core/DeviceAddDialog.java
+++ JSynthLib/core/DeviceAddDialog.java
@@ -2,29 +2,6 @@
 
 package core;
 
-import synthdrivers.KawaiK4.*;
-import synthdrivers.KawaiK5000.*;
-import synthdrivers.EmuProteusMPS.*;
-import synthdrivers.OberheimMatrix.*;
-import synthdrivers.BossDR660.*;
-import synthdrivers.YamahaTG33.*;
-import synthdrivers.YamahaTX81z.*;
-import synthdrivers.EnsoniqESQ1.*;
-import synthdrivers.YamahaDX100.*;
-import synthdrivers.KorgER1.*;
-import synthdrivers.NovationNova1.*;
-import synthdrivers.CasioCZ1000.*;
-import synthdrivers.RolandXV5080.*;       // phil@muqus.com
-import synthdrivers.PeaveyPC1600.*;       // phil@muqus.com
-import synthdrivers.YamahaDX7.*;
-import synthdrivers.KorgWavestation.*;
-import synthdrivers.AlesisQS.*;
-import synthdrivers.RolandMKS50.*;
-import synthdrivers.EnsoniqVFX.*;
-import synthdrivers.SCIProphet600.*;        // KLM
-import synthdrivers.NordLead.*;        // KLM
-import synthdrivers.AccessVirus.*;        // KLM
-import synthdrivers.AlesisA6.*;
 import javax.swing.JList;
 import javax.swing.JScrollPane;
 import javax.swing.JFrame;
@@ -42,43 +19,19 @@ import java.io.*;
 
 public class DeviceAddDialog extends JDialog {
     
-    String [] availibleDevices = {
-	"Access Virus Driver",
-	"Alesis A6 Andromeda Driver",
-        "Alesis QS Series Driver",
-        "Boss DR660 Driver",
-        "Casio CZ-1000 Driver",
-        "Emu Proteus MPS Driver",
-        "Ensoniq ESQ1/ESQm",
-        "Ensoniq VFX",
-        "Kawai K4/K4r Driver",
-        "Kawai K5000 Driver",
-        "Korg ER1 Driver",
-        "Korg Wavestation Driver",
-	"Nord Lead Driver",
-        "Novation Nova 1 Driver",
-        "Oberheim Matrix 1000 Driver",
-        "Oberheim Matrix 6/6R Driver",
-        "Peavey PC1600 Driver", // phil@muqus.com
-        "Roland MKS-50 Driver", //KLM
-        "Roland XV5080 Driver", // phil@muqus.com
-	"Sequential Prophet-600 Driver",
-        "Yamaha DX21 / DX27 / DX100 Driver",
-        "Yamaha DX7 / TX7 Driver",
-        "Yamaha TG33 Driver",
-        "Yamaha TX81z Driver",
-    };
-    
-    JList AvailibleDeviceList;
+    JList AvailableDeviceList;
+	DevicesConfig devConf = null;
     
     public DeviceAddDialog(JFrame Parent) {
         super(Parent,"Synthesizer Device Install",true);
         JPanel container= new JPanel();
         container.setLayout(new BorderLayout());
-        
-        AvailibleDeviceList = new JList(availibleDevices);
-        AvailibleDeviceList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        JScrollPane scrollpane = new JScrollPane(AvailibleDeviceList);
+
+		// Read in list of available devices
+		this.devConf = new DevicesConfig();
+        AvailableDeviceList = new JList(this.devConf.deviceNames());
+        AvailableDeviceList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        JScrollPane scrollpane = new JScrollPane(AvailableDeviceList);
         container.add(scrollpane, BorderLayout.CENTER);
         
         JPanel buttonPanel = new JPanel();
@@ -122,103 +75,13 @@ public class DeviceAddDialog extends JDialog {
     
     void OKPressed() {
         this.setVisible(false);
-        String s=(String)AvailibleDeviceList.getSelectedValue();
-        if (s.equals("Yamaha TG33 Driver")) {
-            PatchEdit.deviceList.add(new YamahaTG33Device());
-            return;
-        }
-        if (s.equals("Yamaha TX81z Driver")) {
-            PatchEdit.deviceList.add(new YamahaTX81zDevice());
-            return;
-        }
-        if (s.equals("Emu Proteus MPS Driver")) {
-            PatchEdit.deviceList.add(new EmuProteusDevice());
-            return;
-        }
-        if (s.equals("Boss DR660 Driver")) {
-            PatchEdit.deviceList.add(new BossDR660Device());
-            return;
-        }
-        if (s.equals("Kawai K4/K4r Driver")) {
-            PatchEdit.deviceList.add(new K4Device());
-            return;
-        }
-        if (s.equals("Oberheim Matrix 1000 Driver")) {
-            PatchEdit.deviceList.add(new OberheimMatrix1000Device());
-            return;
-        }
-        if (s.equals("Oberheim Matrix 6/6R Driver")) {
-            PatchEdit.deviceList.add(new OberheimMatrix6Device());
-            return;
-        }
-        if (s.equals("Kawai K5000 Driver")) {
-            PatchEdit.deviceList.add(new KawaiK5000Device());
-            return;
-        }
-        if (s.equals("Ensoniq ESQ1/ESQm")) {
-            PatchEdit.deviceList.add(new EnsoniqESQ1Device());
-            return;
-        }
-	if (s.equals("Ensoniq VFX")) {
-            PatchEdit.deviceList.add(new EnsoniqVFXDevice());
-            return;
-        }
-        if (s.equals("Yamaha DX21 / DX27 / DX100 Driver")) {
-            PatchEdit.deviceList.add(new YamahaDX100Device());
-            return;
-        }
-        if (s.equals("Yamaha DX7 / TX7 Driver")) {
-            PatchEdit.deviceList.add(new YamahaDX7Device());
-            return;
-        }
-        if (s.equals("Korg ER1 Driver")) {
-            PatchEdit.deviceList.add(new KorgER1Device());
-            return;
-        }
-        if (s.equals("Novation Nova 1 Driver")) {
-            PatchEdit.deviceList.add(new NovationNova1Device());
-            return;
-        }
-        if (s.equals("Peavey PC1600 Driver")) {
-            PatchEdit.deviceList.add(new PeaveyPC1600Device());
-            return;
-        }
-        if (s.equals("Roland XV5080 Driver")) {
-            PatchEdit.deviceList.add(new RolandXV5080Device());
-            return;
-        }
-        if (s.equals("Casio CZ-1000 Driver")) {
-            PatchEdit.deviceList.add(new CasioCZ1000Device());
-            return;
-        }
-        if (s.equals("Korg Wavestation Driver")) {
-            PatchEdit.deviceList.add(new KorgWavestationDevice());
-            return;
-        }
-        if (s.equals("Alesis QS Series Driver")) {
-            PatchEdit.deviceList.add(new AlesisQSDevice());
-            return;
-        }
-        if (s.equals("Roland MKS-50 Driver")) {
-            PatchEdit.deviceList.add(new RolandMKS50Device());
-            return;
-        }
-	if (s.equals("Alesis A6 Andromeda Driver"))                  // KLM
-	    {PatchEdit.deviceList.add(new AlesisA6Device());
-	    return;}
-	if (s.equals("Sequential Prophet-600 Driver"))                  // KLM
-	    {PatchEdit.deviceList.add(new SCIProphet600Device());
-	    return;}
-	if (s.equals("Nord Lead Driver"))                  // KLM
-	    {PatchEdit.deviceList.add(new NordLeadDevice());
-	    return;}
-	if (s.equals("Access Virus Driver"))                  // KLM
-	    {PatchEdit.deviceList.add(new AccessVirusDevice());
-	    return;}
+        String s=(String)AvailableDeviceList.getSelectedValue();
+		Device device = this.devConf.classForDevice(s);
+		if (s!=null) {
+			PatchEdit.appConfig.addDevice(device);
+		}
+	}
 
-
-    }
-    
     void CancelPressed() {
         this.setVisible(false);
     }
