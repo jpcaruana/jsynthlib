287fcf4cd5226357cbc914a16f02646433e73f45
dqueffeulou <dqueffeulou@5361db82-7b37-4a53-9f17-e17e3a93d091>
2003-07-25 18:36:22 +0000

    FS1R drivers for basic operation

---
+++ JSynthLib/synthdrivers/YamahaFS1R/YamahaFS1RDevice.java
@@ -0,0 +1,32 @@
+
+package synthdrivers.YamahaFS1R;
+
+import core.*;
+
+/**
+ * Yamaha FS1R device. 
+ *
+ * @author     Denis Queffeulou mailto:dqueffeulou@free.fr
+ */
+public class YamahaFS1RDevice extends Device
+{
+
+	/**
+	 *  Creates new YamahaFS1RDevice
+	 */
+	public YamahaFS1RDevice()
+	{
+		manufacturerName = "Yamaha";
+		modelName = "FS1R";
+		synthName = "FS1R";
+//		inquiryID = "F07E**06020F05000000000002**F7";
+//		addDriver(new YamahaFS1RBankDriver());
+		addDriver(new YamahaFS1RVoiceDriver());
+		addDriver(new YamahaFS1RPerformanceDriver());
+		addDriver(new YamahaFS1RSystemDriver());
+//		addDriver(new YamahaFS1RFseqDriver());
+        infoText="JSynthLib supports librarian functions on voices/performances patches.\n";
+	}
+
+}
+
