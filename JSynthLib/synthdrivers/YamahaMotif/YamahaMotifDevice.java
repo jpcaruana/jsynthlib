844b1ca4844e6ac2406bb1ab73ced44b042b9c29
gehnen <gehnen@5361db82-7b37-4a53-9f17-e17e3a93d091>
2003-08-18 20:26:47 +0000

    Updated infotext

--- JSynthLib/synthdrivers/YamahaMotif/YamahaMotifDevice.java
+++ JSynthLib/synthdrivers/YamahaMotif/YamahaMotifDevice.java
@@ -7,8 +7,8 @@ package synthdrivers.YamahaMotif;
 
 import core.*;
 /**
- *
  * @author  Ryan Brown
+ * @version $Id$
  */
 public class YamahaMotifDevice extends Device {
     
@@ -18,7 +18,15 @@ public class YamahaMotifDevice extends Device {
         manufacturerName="Yamaha";
         modelName="Motif";
         setSynthName("Motif");
-        infoText="This version has librarian support for single voices (drum and normal), and some editor support for voices. There's some weird stuff going with the element AEG. There's a ton missing from the editor (control sets, lfo's, effects, and filters to name a few). Other than that, there are no known issues or problems with this synthesizer. Please let me (ribrdb@yahoo.com) know if you have any problems. For now it only supports the Motif 6, 7, and 8. I may add support for the Motif Rack and Motif ES series if I feel like it. As soon as I can I will finish the editor support and and librarian support for performances, masters, etc.";
+        infoText="This version has librarian support for single voices (drum "+
+	  "and normal), and some editor support for voices. There's a ton "+
+	  "missing from the editor (control sets, lfo's, effects, and filters "+
+	  "to name a few). Other than that, there are no known issues or "+
+	  "problems with this synthesizer. Please let me (ribrdb@yahoo.com) "+
+	  "know if you have any problems. For now it only supports the Motif "+
+	  "6, 7, and 8. I may add support for the Motif Rack and Motif ES "+
+	  "series if I feel like it. As soon as I can I will finish the "+
+	  "editor support and and support for performances, masters, etc.";
         
 	/*addDriver(0,new YamahaMotifBulkConverter());
         addDriver(new YamahaMotifSingleDriver());
