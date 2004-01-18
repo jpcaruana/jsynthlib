847e223e98486fe9efe56a967f862a5b317c4f98
hayashi <hayashi@5361db82-7b37-4a53-9f17-e17e3a93d091>
2004-01-18 21:38:38 +0000

    define log(String s) method fix indentation more private

--- JSynthLib/core/MidiMonitor.java
+++ JSynthLib/core/MidiMonitor.java
@@ -2,65 +2,67 @@ package core; //TODO org.jsynthlib.midi;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-import javax.swing.text.*;
-import javax.swing.text.html.*;
-import javax.swing.event.*;
-import java.util.*;
-import java.io.*;
-import java.net.*;
-import core.*; //FIXME  core.* will go away eventually - emenaker 3/12/2003
+// import javax.swing.text.*;
+// import javax.swing.text.html.*;
+// import javax.swing.event.*;
+// import java.util.*;
+// import java.io.*;
+// import java.net.*;
+// import core.*; //FIXME  core.* will go away eventually - emenaker 3/12/2003
 public class MidiMonitor extends JDialog
 {
- final MyEditorPane jt;
- public MidiMonitor()
-   {
-     super(PatchEdit.instance,"JSynthLib Midi Monitor",false);
+    private final MyEditorPane jt;
+
+    public MidiMonitor()
+    {
+	super(PatchEdit.instance,"JSynthLib Midi Monitor",false);
         JPanel container= new JPanel();
         container.setLayout (new BorderLayout());
-	jt = new MyEditorPane(); 
+	jt = new MyEditorPane();
 	JScrollPane pane = new JScrollPane();
 	pane.getViewport().add(jt);
-	
+
 	getContentPane().add (pane, BorderLayout.CENTER);
-	 pane.getVerticalScrollBar ().addAdjustmentListener (new AdjustmentListener ()
-        {
-            public void adjustmentValueChanged (AdjustmentEvent e)
-            {
+	pane.getVerticalScrollBar ().addAdjustmentListener (new AdjustmentListener ()
+	    {
+		public void adjustmentValueChanged (AdjustmentEvent e)
+		{
 		    jt.repaint ();
-            }
-        });
+		}
+	    });
 	try {
-//        jt.setContentType("text/html");
-//	FileInputStream in = new FileInputStream("documentation.html");
-//	jt.read(in,(new HTMLEditorKit()).createDefaultDocument());//new HTMLDocument());  
-      jt.setCaretPosition(0);
-	 jt.setEditable(false);
-      jt.setFont(new Font("monospaced", Font.PLAIN, 12));        
-      /*
-     JButton ok = new JButton("Close");
-        ok.addActionListener(new ActionListener() {
-	                       public void actionPerformed(ActionEvent e) {
-                                   OKPressed();
-			       }});
-         getContentPane().add(ok,BorderLayout.SOUTH);
-	getRootPane().setDefaultButton(ok);   
-        */
-        JButton clr = new JButton("Clear");
-       clr.addActionListener(new ActionListener() {
- 	  public void actionPerformed(ActionEvent e) {
- 	    jt.setText("");
- 	  }});
-       getContentPane().add(clr,BorderLayout.SOUTH);
- 
+// 	    jt.setContentType("text/html");
+// 	    FileInputStream in = new FileInputStream("documentation.html");
+// 	    jt.read(in,(new HTMLEditorKit()).createDefaultDocument());//new HTMLDocument());
+	    jt.setCaretPosition(0);
+	    jt.setEditable(false);
+	    jt.setFont(new Font("monospaced", Font.PLAIN, 12));
+	    /*
+	    JButton ok = new JButton("Close");
+	    ok.addActionListener(new ActionListener() {
+		    public void actionPerformed(ActionEvent e) {
+			OKPressed();
+		    }});
+	    getContentPane().add(ok,BorderLayout.SOUTH);
+	    getRootPane().setDefaultButton(ok);
+	    */
+	    JButton clr = new JButton("Clear");
+	    clr.addActionListener(new ActionListener() {
+		    public void actionPerformed(ActionEvent e) {
+			jt.setText("");
+		    }});
+	    getContentPane().add(clr,BorderLayout.SOUTH);
 
-        setSize(500,400);
-	
+	    setSize(500,400);
 
-	 //pane.getVerticalScrollBar().setValue(pane.getVerticalScrollBar().getMinimum());
-	centerDialog();
-	}catch (Exception e) {ErrorMsg.reportError("Error","Error opening Monitor",e);}
-
+// 	    pane.getVerticalScrollBar().setValue(pane.getVerticalScrollBar().getMinimum());
+	    centerDialog();
+	} catch (Exception e) {
+	    ErrorMsg.reportError("Error","Error opening Monitor",e);
 	}
+
+    }
+
     protected void centerDialog() {
         Dimension screenSize = this.getToolkit().getScreenSize();
 	Dimension size = this.getSize();
@@ -73,35 +75,41 @@ public class MidiMonitor extends JDialog
 	this.setLocation(x,y);
     }
 
-  void OKPressed() {
+    void OKPressed() {
  	this.setVisible(false);
-  }                           
-  class MyEditorPane extends JEditorPane
-   {
-	  public void myScrollToReference (String s)  
-	   {
-		   super.scrollToReference(s.substring(1));
-	   } 
-   }
-  void log (int port,boolean in, byte []sysex,int length)
-  {
-      // move the selection at the end of text
-      jt.select(Integer.MAX_VALUE, Integer.MAX_VALUE);
-	 jt.setEditable(true);
-      jt.replaceSelection("Port: " + port + (in ? " RECV " : " XMIT ")
-  			+ length+ " bytes :\n"
- 			+ hexDump(sysex,length) + "\n");
-	 jt.setEditable(false);
-
-  }
-  String hexDump(byte []data,int length){
-    StringBuffer s = new StringBuffer();
-    
-  for (int i = 0; i < length; i++) {
-      String sHex = Integer.toHexString((int) (data[i] & 0xFF));
-      s.append(((sHex.length() == 1) ? "0" : "") + sHex+" ");
-        s.append((i % 20 == 19) ? "\n" : "");
     }
-  return s.toString();
-  }
+
+    class MyEditorPane extends JEditorPane
+    {
+	public void myScrollToReference (String s)
+	{
+	    super.scrollToReference(s.substring(1));
+	}
+    }
+
+    void log (String s)
+    {
+	// move the selection at the end of text
+	jt.select(Integer.MAX_VALUE, Integer.MAX_VALUE);
+	jt.setEditable(true);
+	jt.replaceSelection(s);
+	jt.setEditable(false);
+    }
+
+    void log (int port,boolean in, byte []sysex,int length)
+    {
+	log("Port: " + port + (in ? " RECV " : " XMIT ")
+	    + length+ " bytes :\n" + hexDump(sysex,length) + "\n");
+    }
+
+    String hexDump(byte []data,int length){
+	StringBuffer s = new StringBuffer();
+
+	for (int i = 0; i < length; i++) {
+	    String sHex = Integer.toHexString((int) (data[i] & 0xFF));
+	    s.append(((sHex.length() == 1) ? "0" : "") + sHex+" ");
+	    s.append((i % 20 == 19) ? "\n" : "");
+	}
+	return s.toString();
+    }
 }
