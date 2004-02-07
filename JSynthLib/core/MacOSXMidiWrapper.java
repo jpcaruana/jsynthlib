5a28a790a29aaa1d95744afc056971fe39b59b2e
hayashi <hayashi@5361db82-7b37-4a53-9f17-e17e3a93d091>
2004-02-07 21:44:07 +0000

    make ReadProcImpl.execute push MidiMessage object

--- JSynthLib/core/MacOSXMidiWrapper.java
+++ JSynthLib/core/MacOSXMidiWrapper.java
@@ -13,8 +13,6 @@ import com.apple.audio.units.*;
 import com.apple.audio.util.*;
 import com.apple.audio.*;
 
-
-
 /**
  * Midi wrapper for MacOS X.2
  * This version is not X.1 compatible. Get the older version.
@@ -244,6 +242,9 @@ public class MacOSXMidiWrapper extends MidiWrapper
 		if (oList == null || oList.size() == 0)
 			throw new InvalidMidiDataException("Illegal MIDI input message");
 		// get the older packet
+		MidiMessage msg = (MidiMessage) oList.remove(0);
+
+		/*
 		byte[] oData = (byte[])oList.remove(0);
 // 		System.out.println("readMessage port="+port+" len = "+oData.length);
 
@@ -275,6 +276,7 @@ public class MacOSXMidiWrapper extends MidiWrapper
 								   + oData.length);
 			}
 		}
+		*/
 		logMidi(port, true, msg);
 		return msg;
 	}
@@ -357,6 +359,7 @@ public class MacOSXMidiWrapper extends MidiWrapper
 			Sysex are received in several parts. I put them in a list
 			read by readMessage.
 		*/
+		private boolean inSysex = false; // true during receiving divided Sysex Message
 		public void execute(MIDIInputPort port, MIDIEndpoint srcEndPoint, MIDIPacketList list)
 		{
 			try
@@ -373,6 +376,8 @@ public class MacOSXMidiWrapper extends MidiWrapper
 				{
 					MIDIPacket oPkt = list.getPacket(i);
 					MIDIData oData = oPkt.getData();
+					inSysex = addToList(oList, oData, inSysex);
+					/*
 					byte[] oArray = new byte[oData.getMIDIDataLength()];
 					oData.copyToArray(0,oArray, 0, oData.getMIDIDataLength());
                                       	// Rib Rdb (ribrdb@yahoo.com)
@@ -386,6 +391,7 @@ public class MacOSXMidiWrapper extends MidiWrapper
  					oArray = os.toByteArray();
 					// End ribrdb@yahoo.com
 					oList.add(oArray);
+					*/
 				}
 			}
 			catch(Exception cae)
@@ -393,7 +399,133 @@ public class MacOSXMidiWrapper extends MidiWrapper
 				cae.printStackTrace();
 			}
 		}
-	}
 
+		/**
+		 * convert a MIDIData objects into MidiMessage objects
+		 * and add them to the input list, <code>oList</code>.
+		 *
+		 * @author Hiroo Hayashi
+		 * @See javax.sound.midi.SystemMessage
+		 */
+		private boolean addToList (List oList, MIDIData oData, boolean inSysex) {
+			int status = (int) (oData.getByteAt(0) & 0xff);
+			int len = oData.getMIDIDataLength();
+			if (status == SysexMessage.SYSTEM_EXCLUSIVE || inSysex) {
+				byte[] d = new byte[len];
+				oData.copyToArray(0, d, 0, len);
+				ErrorMsg.reportStatus("Sysex: " + MidiUtil.hexDump(d, 0, -1, 16));
+				SysexMessage msg = new SysexMessage();
+				try {
+					if (status == SysexMessage.SYSTEM_EXCLUSIVE) {
+						msg.setMessage(d, len);
+					} else {
+						msg.setMessage(SysexMessage.SPECIAL_SYSTEM_EXCLUSIVE, d, len);
+					}
+				} catch (InvalidMidiDataException e) {
+					// let getMessage cause InvalidMidiDataException
+					msg = null;
+				}
+				oList.add(msg);
+				// return true if not terminated by EOX
+				return ((int) (d[len - 1] & 0xff)) != ShortMessage.END_OF_EXCLUSIVE;
+			} else {
+				// for debugging
+				byte[] d = new byte[len];
+				oData.copyToArray(0, d, 0, len);
+				ErrorMsg.reportStatus("Short: " + MidiUtil.hexDump(d, 0, -1, 16));
+				// Single Messages
+				for (int i = 0; i < len; i++) {
+					status = (int) (oData.getByteAt(i) & 0xff);
+					// ignore System Real Time Message and data byte
+					if ((status & 0xf8) == 0xf8 || (status & 0x80) == 0)
+						continue;
+
+					ShortMessage msg = new ShortMessage();
+					try {
+						int data1, data2;
+
+						switch (status < 0xf0 ? status & 0xf0 : status) {
+						case ShortMessage.NOTE_OFF:		// 0x8n
+						case ShortMessage.NOTE_ON:		// 0x9n
+						case ShortMessage.POLY_PRESSURE:	// 0xAn
+						case ShortMessage.CONTROL_CHANGE:	// 0xBn
+						case ShortMessage.PITCH_BEND:		// 0xEn
+						case ShortMessage.SONG_POSITION_POINTER: // 0xf2
+							data1 = (int) (oData.getByteAt(++i) & 0xff);
+							data2 = (int) (oData.getByteAt(++i) & 0xff);
+							msg.setMessage(status, data1, data2);
+							break;
+
+						case ShortMessage.PROGRAM_CHANGE:	// 0xCn
+						case ShortMessage.CHANNEL_PRESSURE:	// 0xDn
+						case ShortMessage.MIDI_TIME_CODE:	// 0xf1
+						case ShortMessage.SONG_SELECT:		// 0xf3
+							data1 = (int) (oData.getByteAt(++i) & 0xff);
+							msg.setMessage(status, data1, 0);
+							break;
+
+						case ShortMessage.TUNE_REQUEST:		// 0xf6
+							msg.setMessage(status);
+							break;
+
+						case 0xf4: // undefined
+						case 0xf5: // undefined
+						case ShortMessage.END_OF_EXCLUSIVE:	// 0xf7
+							continue; // ignore
+						default:
+							msg = null;
+							ErrorMsg.reportStatus("Illegal Status:" + status
+									      + ", i=" + i);
+						}
+					} catch (InvalidMidiDataException e) {
+						// let getMessage cause InvalidMidiDataException
+						msg = null;
+						ErrorMsg.reportStatus("InvalidMidiData:" + status
+								      + ", i=" + i);
+					} catch (Exception e) {
+						// What does getByteAt() throw for out of range?
+						msg = null;
+						ErrorMsg.reportStatus("???:" + status + ", i=" + i
+								      + ": " + e);
+					}
+					oList.add(msg);
+				}
+				return false;
+			}
+		}
+	}
 }
+/*
+Here's what the api says:
+struct MIDIPacket {
+  MIDITimeStamp  timeStamp;
+  UInt16         length;
+  Byte           data[256];
+};
+One or more MIDI events occuring at a particular time.
+
+Fields
+
+timeStamp
+
+The time at which the events occurred, if receiving MIDI, or, if
+sending MIDI, the time at which the events are to be played. Zero
+means "now." The time stamp applies to the first MIDI byte in the
+packet.
+
+length
+
+The number of valid MIDI bytes which follow, in data. (It may be
+larger than 256 bytes if the packet is dynamically allocated.)
+
+data
+
+A variable-length stream of MIDI messages. Running status is not
+allowed. In the case of system-exclusive messages, a packet may only
+contain a single message, or portion of one, with no other MIDI
+events. The MIDI messages in the packet must always be complete,
+except for system-exclusive. Single-byte MIDI realtime messages may
+not occur between the status and data bytes of other messages, except
+in system-exclusive.
+*/
 //(setq c-basic-offset 8)
