72aa123f6ad65e0833dfede6c330739991117e09
hayashi <hayashi@5361db82-7b37-4a53-9f17-e17e3a93d091>
2004-03-21 19:58:48 +0000

    setXxxx() does more job. add masterInEnable remove getMasterControllerEnable() add JSLMidiDevice (midiIn, midiOut, midiMasterIn, midiFaderIn) use TreeSet instead of HashSet to reserve order make getOSName() etc. static

--- JSynthLib/core/AppConfig.java
+++ JSynthLib/core/AppConfig.java
@@ -12,7 +12,8 @@ package core;
 import java.util.Properties;
 import java.util.ArrayList;
 import java.util.Set;
-import java.util.HashSet;
+import java.util.TreeSet;
+import java.util.Vector;
 //import java.util.Collection;
 import java.util.Iterator;
 import java.util.Arrays;
@@ -23,6 +24,7 @@ import java.io.OutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.FileNotFoundException;
+import javax.swing.UIManager;
 
 public class AppConfig implements Storable {
 
@@ -38,13 +40,26 @@ public class AppConfig implements Storable {
    	private int masterController     = 0;
    	private int lookAndFeel          = 0;
 	private int guiStyle		 = MacUtils.isMac() ? 1 : 0;
-   	private int midiPlatform         = 0;
+   	private int midiPlatform	 = 0;
+   	private boolean masterInEnable   = false;
    	private int faderPort            = 0;
    	private boolean faderEnable      = false;
    	private int[] faderController    = new int[Constants.NUM_FADERS];
    	private int[] faderChannel       = new int[Constants.NUM_FADERS];
 
 	private ArrayList deviceList = new ArrayList();
+
+	/** MidiDevice.Info.name|vendor */
+	private JSLMidiDevice midiIn;
+	private JSLMidiDevice midiOut;
+	private JSLMidiDevice midiMasterIn;
+	private JSLMidiDevice midiFaderIn;
+	private static MidiWrapper midiWrapper	 = null;
+
+	static Vector midiWrappers;
+	{
+		midiWrappers = MidiWrapper.getSuitableWrappers();
+	}
 
 	/**
 	 * Constructor
@@ -55,7 +70,7 @@ public class AppConfig implements Storable {
 	/**
 	 * This one loads the settings from the config file.
 	 */
-	public boolean loadPrefs() {
+	boolean loadPrefs() {
 		/*
 		 * These are handled by the individual ConfigPanels now - emenaker 2003.03.25
 		 appConfig.setInitPortIn(0);
@@ -100,7 +115,7 @@ public class AppConfig implements Storable {
 	 * This routine just saves the current settings in the config
 	 * file. Its called when the user quits the app.
 	 */
-	public void savePrefs() {
+	void savePrefs() {
 		try {
 			// Save the appconfig
 			store();
@@ -140,12 +155,18 @@ public class AppConfig implements Storable {
 	/** Getter for initPortIn */
 	public int getInitPortIn() { return this.initPortIn; };
 	/** Setter for initPortIn */
-	public void setInitPortIn(int initPortIn) { this.initPortIn = initPortIn; };
+	public void setInitPortIn(int initPortIn) {
+	    if (initPortIn < 0) initPortIn = 0;
+	    this.initPortIn = initPortIn;
+	};
 
 	/** Getter for initPortOut */
 	public int getInitPortOut() { return this.initPortOut; };
 	/** Setter for initPortOut */
-	public void setInitPortOut(int initPortOut) { this.initPortOut = initPortOut; };
+	public void setInitPortOut(int initPortOut) {
+	    if (initPortOut < 0) initPortOut = 0;
+	    this.initPortOut = initPortOut;
+	};
 
 	/** Getter for note */
 	public int getNote() { return this.note; };
@@ -165,16 +186,24 @@ public class AppConfig implements Storable {
 	/** Getter for masterController */
 	public int getMasterController() { return this.masterController; };
 	/** Setter for masterController */
-	public void setMasterController(int masterController) { this.masterController = masterController; };
-	/** Return true if master controller is enabled. */
-	public boolean getMasterControllerEnable() {
-	    return (0 <= masterController);
-	}
+	public void setMasterController(int masterController) {
+	    if (masterController < 0) masterController = 0;
+	    this.masterController = masterController;
+	};
 
 	/** Getter for lookAndFeel */
 	public int getLookAndFeel() { return this.lookAndFeel; };
 	/** Setter for lookAndFeel */
-	public void setLookAndFeel(int lookAndFeel) { this.lookAndFeel = lookAndFeel; };
+	public void setLookAndFeel(int lookAndFeel) {
+	    this.lookAndFeel = lookAndFeel;
+	    UIManager.LookAndFeelInfo [] installedLF;
+	    installedLF = UIManager.getInstalledLookAndFeels();
+	    try {
+		UIManager.setLookAndFeel(installedLF[lookAndFeel].getClassName());
+	    } catch (Exception e) {
+		ErrorMsg.reportStatus(e.toString());
+	    }
+	};
 
 	/** Getter for guiStyle */
 	public int getGuiStyle() { return this.guiStyle; };
@@ -183,13 +212,65 @@ public class AppConfig implements Storable {
 
 	/** Getter for midiPlatform */
 	public int getMidiPlatform() { return this.midiPlatform; };
+
 	/** Setter for midiPlatform */
-	public void setMidiPlatform(int midiPlatform) { this.midiPlatform = midiPlatform; };
+	public void setMidiPlatform(int midiPlatform) {
+	    if (midiPlatform < 0) midiPlatform = 0;
+	    MidiWrapper mw = (MidiWrapper) midiWrappers.elementAt(midiPlatform);
+	    ErrorMsg.reportStatus("Something selected \"" + mw + "\"");
+	    MidiWrapper currentDriver = getMidiWrapper();
+	    if(currentDriver != null) {
+		if(currentDriver.toString().equals(mw.toString())) {
+		    ErrorMsg.reportStatus("We're already using that driver");
+		    return;
+		}
+		currentDriver.close();
+	    }
+	    // initialize selected MIDI wrapper
+	    ErrorMsg.reportStatus("Initializing driver:" + mw.toString());
+	    try {
+		mw.init(getInitPortIn(), getInitPortOut());
+
+		this.midiPlatform = midiPlatform;
+		PatchEdit.MidiIn = PatchEdit.MidiOut = midiWrapper = mw;
+	    } catch (DriverInitializationException e) {
+		core.ErrorMsg.reportError
+		    ("Error",
+		     "There was an error initializing the MIDI Wrapper!", e);
+		//e.printStackTrace ();
+		setMidiPlatform(0); // DoNothingMidiWrapper
+	    } catch (Exception e) {
+		core.ErrorMsg.reportError
+		    ("Error",
+		     "There was an unspecified problem while initializing the driver!", e);
+		e.printStackTrace ();
+		setMidiPlatform(0); // DoNothingMidiWrapper
+	    }
+	}
+
+	/**
+	 * This returns the currently-selected midi wrapper. At present,
+	 * it's only used by the main app to obtain the midi driver after
+	 * the initial instantiation (ie, before it ever gets a
+	 * midiDriverChanged() callback).
+	 * @return
+	 */
+	public static MidiWrapper getMidiWrapper() {
+		return(midiWrapper);
+	}
+
+	/** Getter for masterInEnable */
+	public boolean getMasterInEnable() { return this.masterInEnable; };
+	/** Setter for masterInEnable */
+	public void setMasterInEnable(boolean masterInEnable) { this.masterInEnable = masterInEnable; };
 
 	/** Getter for faderPort */
 	public int getFaderPort() { return this.faderPort; };
 	/** Setter for faderPort */
-	public void setFaderPort(int faderPort) { this.faderPort = faderPort; };
+	public void setFaderPort(int faderPort) {
+	    if (faderPort < 0) faderPort = 0;
+	    this.faderPort = faderPort;
+	};
 
 	/** Getter for faderEnable */
 	public boolean getFaderEnable() { return this.faderEnable; };
@@ -283,31 +364,51 @@ public class AppConfig implements Storable {
 // 	    return -1;		// throw error !!!FIXIT!!!
 	}
 
-	// Returns the "os.name" system property - emenaker 2003.03.13
-	public String getOSName() {
-		return(getSystemProperty("os.name"));
-	}
+	/** Getter/setter for MidiDevice.info */
+	// These are experimental code.
+	public JSLMidiDevice getMidiIn() { return midiIn; }
+	public JSLMidiDevice getMidiOut() { return midiOut; }
+	public JSLMidiDevice getMidiMasterIn() { return midiMasterIn; }
+	public JSLMidiDevice getMidiFaderIn() { return midiFaderIn; }
+	public void setMidiIn(JSLMidiDevice midiIn) { this.midiIn = midiIn; }
+	public void setMidiOut(JSLMidiDevice midiOut) { this.midiOut = midiOut; }
+	public void setMidiMasterIn(JSLMidiDevice midiMasterIn) { this.midiMasterIn = midiMasterIn; }
+	public void setMidiFaderIn(JSLMidiDevice midiFaderIn) { this.midiFaderIn = midiFaderIn; }
 
-	// Returns the "java.specification.version" system property - emenaker 2003.03.13
-	public String getJavaSpecVersion() {
-		return(getSystemProperty("java.specification.version"));
-	}
 
-	// Looks up a system property and returns "" on exceptions
-	private String getSystemProperty(String key) {
-		try {
-			return(System.getProperty(key));
-		} catch(Exception e) {}
-		return("");
+	// temporally hack
+    /*
+	void setMidiDevices() {
+	    JavasoundMidiWrapper jw = (JavasoundMidiWrapper) PatchEdit.MidiOut;
+	    for (int i = 0; i < deviceList.size(); i++) {
+		Device dev = (Device) deviceList.get(i);
+		int port = dev.getPort();
+		setMidiOut(jw.getOutputDevice(port));
+		int inPort = dev.getInPort();
+		setMidiIn(jw.getInputDevice(inPort));
+	    }
 	}
-
+    */
 	// For Storable interface
-
 	private static final String[] storedPropertyNames = {
-		"libPath", "sysexPath", "initPortIn", "initPortOut", "note",
-		"velocity", "delay", "masterController", "lookAndFeel",
-		"guiStyle", "midiPlatform", "faderPort", "faderEnable",
-		"faderController","faderChannel", "device"
+		"libPath", "sysexPath", "note",
+		"velocity", "delay", "lookAndFeel", "guiStyle",
+		"faderEnable", "faderController","faderChannel",
+		"masterInEnable",
+		"initPortIn", "initPortOut", "masterController", "faderPort",
+		// must be restored after initPortIn and initPortOut
+		"midiPlatform", "device",
+	};
+	private static final String[] storedPropertyNamesNew = {
+		"libPath", "sysexPath", "note",
+		"velocity", "delay", "lookAndFeel", "guiStyle",
+		"faderEnable", "faderController","faderChannel",
+		"masterInEnable",
+		// for backward compatibility.
+		"initPortIn", "initPortOut", "masterController", "faderPort",
+		"midiPlatform", "device",
+		// for javax.sound
+		"midiIn", "midiOut", "midiMasterIn", "midiFaderIn",
 	};
 
 	/**
@@ -315,8 +416,12 @@ public class AppConfig implements Storable {
 	 * @return a Set of field names
 	 */
 	public Set storedProperties() {
-		HashSet set = new HashSet();
-		set.addAll(Arrays.asList(this.storedPropertyNames));
+		// TreeSet reserves the restore order
+		TreeSet set = new TreeSet();
+		if (PatchEdit.newMidiAPI)
+		    set.addAll(Arrays.asList(storedPropertyNamesNew));
+		else
+		    set.addAll(Arrays.asList(storedPropertyNames));
 		return set;
 	}
 
@@ -327,5 +432,25 @@ public class AppConfig implements Storable {
 		// do nothing - we don't need any special code to execute after restore
 // 		ErrorMsg.reportStatus("AppConfig: " + deviceList);
 	}
+
+	// Shall we define JSLUtil for these mothods?
+	// Returns the "os.name" system property - emenaker 2003.03.13
+	public static String getOSName() {
+		return(getSystemProperty("os.name"));
+	}
+
+	// Returns the "java.specification.version" system property - emenaker 2003.03.13
+	public static String getJavaSpecVersion() {
+		return(getSystemProperty("java.specification.version"));
+	}
+
+	// Looks up a system property and returns "" on exceptions
+	private static String getSystemProperty(String key) {
+		try {
+			return(System.getProperty(key));
+		} catch(Exception e) {}
+		return("");
+	}
 }
 //(setq c-basic-offset 8)
+//(setq c-basic-offset 4)
