fbee28fb8afdf1f94f96c03eb119e8ad8bea9016
bklock <bklock@5361db82-7b37-4a53-9f17-e17e3a93d091>
2004-03-29 20:57:45 +0000

    GUI complete and start if implementation for Uploading Patch Dialog

--- JSynthLib/core/UploadPatchDialog.java
+++ JSynthLib/core/UploadPatchDialog.java
@@ -16,55 +16,97 @@ import java.util.*;
 import java.awt.event.*;
 import java.awt.*;
 import java.io.*;
-
+import java.net.*;
 public class UploadPatchDialog extends JDialog {
-  final JLabel l1;
+  final JTextField t1;
+  final JTextField t2;
+  final JTextArea  t3;
+  final JTextField t4;
+  final JTextField t5;
+  final JPasswordField t6;
+
+
   public UploadPatchDialog(JFrame Parent) {
-        super(Parent,"Upload Patch to Repository",false);
- 	JPanel container= new JPanel();
-        container.setLayout (new BorderLayout());
-        JPanel p4 = new JPanel();
-        p4.setLayout (new ColumnLayout());
-	l1 = new JLabel("Patch Type: ");
-
-	JPanel buttonPanel = new JPanel();
-	JPanel buttonPanel2=new JPanel();
-	JButton upload = new JButton("Upload");
-	upload.addActionListener(new ActionListener() {
-	                       public void actionPerformed(ActionEvent e) {
-                                try {
-					PatchBasket library=(PatchBasket)JSLDesktop.getSelectedFrame();
-					Patch q	= library.GetSelectedPatch();
-					uploadPatch(q);
-			       }catch (Exception ex){JOptionPane.showMessageDialog(null, "Patch Must be Focused","Error", JOptionPane.ERROR_MESSAGE);}
-			       }});
-        buttonPanel2.add(upload);
-	JButton play = new JButton("Play");
-	play.addActionListener(new ActionListener() {
-	                       public void actionPerformed(ActionEvent e) {
-                                 play();
-			       }});
-        buttonPanel2.add(play);
-
-	JButton ok = new JButton("Cancel");
-        ok.addActionListener(new ActionListener() {
-	                       public void actionPerformed(ActionEvent e) {
-                                   OKPressed();
-			       }});
-	buttonPanel.add( ok );
-	getRootPane().setDefaultButton(ok);
-        p4.add(l1);
-
-	container.add(p4,BorderLayout.NORTH);
-	container.add(buttonPanel2,BorderLayout.CENTER);
-	container.add(buttonPanel,BorderLayout.SOUTH);
-	getContentPane().add(container);
-       // setSize(400,300);
-        pack();
-	centerDialog();
+      super(Parent,"Upload Patch to Repository",false);
 
 
-  }
+    String patchType;
+    PatchBasket library=(PatchBasket)JSLDesktop.getSelectedFrame();
+    Patch q	= library.GetSelectedPatch(); 
+    
+    JPanel container= new JPanel();
+    container.setLayout (new BorderLayout());
+    JPanel p4 = new JPanel();   
+    p4.setLayout (new ColumnLayout());
+    JLabel l1 = new JLabel("Patch Type: ");
+    t1 = new JTextField(20);
+    JLabel l2 = new JLabel("Patch Name: ");
+    t2 = new JTextField(20);
+    t2.setText(q.getDriver().getPatchName(q));
+    t1.setEditable(false);
+
+    patchType= q.getDriver().getManufacturerName();
+    patchType+=" ";
+    patchType+=q.getDriver().getModelName();
+    t1.setText(patchType);
+	
+    JLabel l4 = new JLabel("Repository:");
+     t4=new JTextField(20);
+    t4.setText(PatchEdit.appConfig.getRepositoryURL());
+    JLabel l5 = new JLabel("Contributor: ");
+     t5=new JTextField(20);
+    t5.setText(PatchEdit.appConfig.getRepositoryUser());
+    JLabel l6 = new JLabel("Contributor Password: ");
+     t6=new JPasswordField(20);
+    t6.setText(PatchEdit.appConfig.getRepositoryPass());
+
+    JLabel l3 = new JLabel("Description: ");
+     t3 = new JTextArea(4,50);
+     t3.setText(q.getComment());
+    
+    
+    JPanel buttonPanel = new JPanel();
+    JButton upload = new JButton("Upload");
+    upload.addActionListener(new ActionListener() {
+	    public void actionPerformed(ActionEvent e) {
+		try {
+		    PatchBasket library=(PatchBasket)JSLDesktop.getSelectedFrame();
+		    Patch q	= library.GetSelectedPatch();
+		    uploadPatch(q);
+		}catch (Exception ex){JOptionPane.showMessageDialog(null, "Patch Must be Focused","Error", JOptionPane.ERROR_MESSAGE);}
+	    }});
+    buttonPanel.add(upload);
+    JButton play = new JButton("Play");
+    play.addActionListener(new ActionListener() {
+	    public void actionPerformed(ActionEvent e) {
+		play();
+	    }});
+    buttonPanel.add(play);
+    
+    JButton ok = new JButton("Cancel");
+    ok.addActionListener(new ActionListener() {
+	    public void actionPerformed(ActionEvent e) {
+		OKPressed();
+	    }});
+    buttonPanel.add( ok );
+    getRootPane().setDefaultButton(ok);
+    p4.add(l4);p4.add(t4);
+    p4.add(l5);p4.add(t5);
+    p4.add(l6);p4.add(t6);
+    p4.add(l1);p4.add(t1);
+    p4.add(l2);p4.add(t2);
+    p4.add(l3);p4.add(t3);
+  
+    
+    container.add(p4,BorderLayout.NORTH);
+    container.add(buttonPanel,BorderLayout.SOUTH);
+    getContentPane().add(container);
+    // setSize(400,300);
+    pack();
+    centerDialog();
+    
+    
+}
    public void show()
    {
        super.show();
@@ -88,7 +130,99 @@ void OKPressed() {
   }
 void uploadPatch(Patch p)
 {
+    String patchType=t1.getText();
+    String patchName=t2.getText();
+    String desc=t3.getText();
+    String repository=t4.getText();
+    String userName=t5.getText();
+    String passwd=t6.getText();
+    byte [] sysex = p.sysex;
+    if (patchName.length()<4)
+	ErrorMsg.reportError("Error","Patch Name must be at least 4 characters.");
+    else if (desc.length()<20)
+	ErrorMsg.reportError("Error","Description must be at least 20 characters.");
+    else if (userName.length()<1)
+	ErrorMsg.reportError("Error","Please supply a Contributer's User Name");
+    else if (passwd.length()<1)
+	ErrorMsg.reportError("Error","Please supply the Contributer's Password for the Repository");
+    else if (repository.length()<5)
+	ErrorMsg.reportError("Error","Please supply the Repository URL Address");
+    else
+	{
+	    String mime = new String();
+	    mime = mimeBoundary();
+	    mime += makeMime("name",patchName);
+	    mime += mimeBoundary();
+	    mime += makeMime("synthName",patchType);
+	    mime += mimeBoundary();
+	    try{
+	    String filestr=new String (sysex,"ISO-8859-1");	    
+	    mime += makeMime("file",filestr);
+	    } catch (Exception e) {System.out.println("UploadPatchDialog encoding failed.");}
+	    mime += mimeBoundary();
+	    mime += makeMime("username",userName);
+	    mime += mimeBoundary();
+	    mime += makeMime("pass",passwd);
+	    mime += mimeBoundary();
+	    mime += makeMime("comment",desc);
+	    mime += mimeBoundary();
+	    mime += makeMime("synth","1");
+	    mime += mimeBoundary();
+	    mime += makeMime("send","Upload");
+	    mime += mimeBoundary();
+	    mime += "\r\n\r\n";
+	    System.out.println(mime);
+	    postData(repository,mime);
+	}
+	
+
 }
+
+    String makeMime(String name1, String body1)
+    {
+	String s = new String();
+	s = "Content-Disposition: form-data; name=\"" + name1 + "\"\r\n\r\n";
+	s +=body1;
+	s+="\r\n";
+	return s;
+	
+    }
+
+    void postData(String repository,String mime)
+    {
+	try{
+	    URL url = new URL(repository+"/cgi-bin/upload.cgi");
+	    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+	    conn.setDoInput(true);
+	    conn.setDoOutput(true);
+	    conn.setUseCaches(false);
+	    conn.setRequestMethod("POST");
+	    conn.setRequestProperty("Connection", "Close");
+	    conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=-----------------------------18042893838469308861681692777");
+	    DataOutputStream os = new DataOutputStream(conn.getOutputStream());
+	    os.writeBytes(mime);
+	    os.flush();
+	    os.close();
+	    DataInputStream in = new DataInputStream(conn.getInputStream());
+	    String str;
+	    while (( str = in.readLine()) != null)
+		{
+		    		    System.out.println("Server: "+str);
+		    System.out.println("");
+
+		}
+   in.close();    
+	    
+	} catch (Exception e){ ErrorMsg.reportStatus(e);}
+	
+
+    }
+
+    String mimeBoundary()
+    {
+	return "-----------------------------18042893838469308861681692777\r\n";
+    }
+
   void play()
  {
     try{
