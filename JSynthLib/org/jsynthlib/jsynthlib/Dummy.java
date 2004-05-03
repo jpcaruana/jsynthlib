0b07653c55d1b4eef9c737427049271f12e7aa15
ribrdb <ribrdb@5361db82-7b37-4a53-9f17-e17e3a93d091>
2004-05-03 16:46:12 +0000

    Dummy class to make the package name of stored preferences be under org.jsynthlib instead of just core.  It is org.jsynthlib.jsynthlib because OS X's preferences requires a class name of the form (2 or more part domain name).project.(packages and class)

---
+++ JSynthLib/org/jsynthlib/jsynthlib/Dummy.java
@@ -0,0 +1,9 @@
+/*
+ * Created on May 2, 2004
+ *
+ * Dummy class so preferences will be stored under org.jsynthlib instead of just core
+ */
+package org.jsynthlib.jsynthlib;
+
+public class Dummy {
+}
