549abc04158510fc3381ec5cb1a456956ad2decf
ttittmann <ttittmann@5361db82-7b37-4a53-9f17-e17e3a93d091>
2004-05-03 18:42:48 +0000

    added README/build.sh files for LinuxCharDev MidiProvider

---
+++ JSynthLib/midiprovider/LinuxCharDev/build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo Compiling...
+for n in LinuxCharDevMidiProvider/*.java; do javac -g $n; done
+echo Building jar...
+jar cf ../LinuxCharDev.jar META-INF/services/javax.sound.midi.spi.MidiDeviceProvider LinuxCharDevMidiProvider/*.class
+echo Done.
