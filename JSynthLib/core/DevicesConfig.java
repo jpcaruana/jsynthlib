2ac21de1dbc17832c46701e802a9a96156a8c609
hayashi <hayashi@5361db82-7b37-4a53-9f17-e17e3a93d091>
2004-05-09 03:43:00 +0000

    reindent.

--- JSynthLib/core/DevicesConfig.java
+++ JSynthLib/core/DevicesConfig.java
@@ -20,150 +20,137 @@ import java.io.IOException;
 
 public class DevicesConfig {
 
-	/* Properties representing config file */
-	private Properties configProps = new Properties();
-	/* Properties representing devices and class names */
-	private Properties deviceProps = new Properties();
-	/* ArrayList of device names */
-	private ArrayList deviceNames = new ArrayList();
-        /* Properties representing the ID-Strings and device names */
-        private Properties deviceIDProps = new Properties();
+    /* Properties representing config file */
+    private Properties configProps = new Properties();
+    /* Properties representing devices and class names */
+    private Properties deviceProps = new Properties();
+    /* ArrayList of device names */
+    private ArrayList deviceNames = new ArrayList();
+    /* Properties representing the ID-Strings and device names */
+    private Properties deviceIDProps = new Properties();
 
-	/**
-	 * Main method for debugging - print out all configured Devices
-	 */
-	public static void main(String args[]) {
-		DevicesConfig devConf = new DevicesConfig();
-		devConf.printAll();
+    /**
+     * Main method for debugging - print out all configured Devices
+     */
+    public static void main(String[] args) {
+	DevicesConfig devConf = new DevicesConfig();
+	devConf.printAll();
+    }
+
+    /**
+     * Constructor
+     */
+    public DevicesConfig() {
+	// Load properties file
+	InputStream in = this.getClass().getResourceAsStream("/" + Constants.RESOURCE_NAME_DEVICES_CONFIG);
+	//Properties configProps = new Properties();
+	try {
+	    configProps.load(in);
+	} catch (IOException e) {
+	    ErrorMsg.reportError("Failed Loading Devices",
+				 "Failed loading devices property file. "
+				 + "Resource " + Constants.RESOURCE_NAME_DEVICES_CONFIG
+				 + " cannot be loaded", e);
+	} finally {
+	    try {
+		in.close();
+	    } catch (IOException e) {
+		// do nothing
+	    }
 	}
 
-	/**
-	 * Constructor
-	 */
-	public DevicesConfig() {
+	//configProps.propertyNames()
+	Enumeration propNames = configProps.propertyNames();
+	while (propNames.hasMoreElements()) {
+	    String propName = (String) propNames.nextElement();
+	    if (propName.startsWith(Constants.PROP_PREFIX_DEVICE_NAME)) {
+		String deviceKey = propName.substring(Constants.PROP_PREFIX_DEVICE_NAME.length());
+		String deviceName = configProps.getProperty(propName);
+		String deviceClass = configProps.getProperty(
+							     Constants.PROP_PREFIX_DEVICE_CLASS + deviceKey);
 
-		// Load properties file
-		InputStream in = this.getClass().getResourceAsStream("/" + Constants.RESOURCE_NAME_DEVICES_CONFIG);
-		//Properties configProps = new Properties();
-		try {
-			configProps.load(in);
+		if ((deviceName == null) || (deviceClass == null)) {
+		    ErrorMsg.reportError("Failed loading Devices",
+					 "Config file inconsistency found "
+					 + "for '" + deviceKey + "' device");
+		} else {
+		    deviceProps.setProperty(deviceName, deviceClass);
+		    this.deviceNames.add(deviceName);
 		}
-		catch (IOException e) {
-			ErrorMsg.reportError("Failed Loading Devices",
-								 "Failed loading devices property file. "
-								 + "Resource " + Constants.RESOURCE_NAME_DEVICES_CONFIG
-								 + " cannot be loaded", e);
-		}
-		finally {
-			try {
-				in.close();
-			}
-			catch (IOException e) {
-				// do nothing
-			}
-		}
+	    }
+	    if (propName.startsWith(Constants.PROP_PREFIX_ID_STRING)) {
+		String deviceKey = propName.substring(Constants.PROP_PREFIX_ID_STRING.length());
+		String deviceName = configProps.getProperty(Constants.PROP_PREFIX_DEVICE_NAME + deviceKey);
+		String IDString = configProps.getProperty(Constants.PROP_PREFIX_ID_STRING + deviceKey);
 
-		//configProps.propertyNames()
-		Enumeration propNames = configProps.propertyNames();
-		while (propNames.hasMoreElements())
-		{
-			String propName = (String)propNames.nextElement();
-			if (propName.startsWith(Constants.PROP_PREFIX_DEVICE_NAME)) {
-				String deviceKey = propName.substring(
-					Constants.PROP_PREFIX_DEVICE_NAME.length());
-				String deviceName = configProps.getProperty(propName);
-				String deviceClass = configProps.getProperty(
-					Constants.PROP_PREFIX_DEVICE_CLASS + deviceKey);
-
-				if ((deviceName==null) || (deviceClass==null)) {
-					ErrorMsg.reportError("Failed loading Devices",
-										 "Config file inconsistency found "
-										 + "for '" + deviceKey + "' device");
-				}
-				else {
-					deviceProps.setProperty(deviceName, deviceClass);
-					this.deviceNames.add(deviceName);
-				}
-			}
-                        if (propName.startsWith(Constants.PROP_PREFIX_ID_STRING)) {
-				String deviceKey = propName.substring(
-					Constants.PROP_PREFIX_ID_STRING.length());
-				String deviceName = configProps.getProperty(Constants.PROP_PREFIX_DEVICE_NAME+deviceKey);
-				String IDString = configProps.getProperty(
-					Constants.PROP_PREFIX_ID_STRING + deviceKey);
-
-				if ((deviceName==null) || (IDString==null)) {
-					ErrorMsg.reportError("Failed loading Devices",
-										 "Config file inconsistency found "
-										 + "for '" + deviceKey + "' ID string");
-				}
-				else {
-					deviceIDProps.setProperty(IDString,deviceName);
-					//this.deviceNames.add(deviceName);
-				}
-			}
+		if ((deviceName == null) || (IDString == null)) {
+		    ErrorMsg.reportError("Failed loading Devices",
+					 "Config file inconsistency found "
+					 + "for '" + deviceKey + "' ID string");
+		} else {
+		    deviceIDProps.setProperty(IDString, deviceName);
+		    //this.deviceNames.add(deviceName);
 		}
-		Collections.sort(this.deviceNames);
+	    }
 	}
+	Collections.sort(this.deviceNames);
+    }
 
-	/**
-	 * Return the device names
-	 * @return the names of configured devices
-	 */
-	public String[] deviceNames() {
-		String[] retVal = new String[this.deviceNames.size()];
-		return (String[])this.deviceNames.toArray(retVal);
+    /**
+     * Return the device names
+     * @return the names of configured devices
+     */
+    public String[] deviceNames() {
+	String[] retVal = new String[this.deviceNames.size()];
+	return (String[]) this.deviceNames.toArray(retVal);
+    }
+
+    public Enumeration IDStrings() {
+	return deviceIDProps.keys();
+    }
+
+    /**
+     * Given a device name, return its class name
+     * @param deviceName the name of the device
+     * @return the class name
+     */
+    public String classNameForDevice(String deviceName) {
+	return this.deviceProps.getProperty(deviceName);
+    }
+
+    public Device classForIDString(String IDString) {
+	Device device = null;
+	String deviceName = this.deviceIDProps.getProperty(IDString);
+	return this.classForDevice(deviceName);
+    }
+
+    /**
+     * Given a device name, return an instance of its class
+     * @param deviceName the name of the device
+     * @return an instance of the device's class
+     */
+    public Device classForDevice(String deviceName) {
+	Device device = null;
+	String className = this.deviceProps.getProperty(deviceName);
+	try {
+	    Class deviceClass = Class.forName(className);
+	    device = (Device) deviceClass.newInstance();
+	} catch (Exception e) {
+	    ErrorMsg.reportError("Failed to find class for device",
+				 "Failed to find class for device '"
+				 + deviceName + "'");
 	}
+	return device;
+    }
 
-        public Enumeration IDStrings() {
-            return deviceIDProps.keys();
-        }
-	/**
-	 * Given a device name, return its class name
-	 * @param deviceName the name of the device
-	 * @return the class name
-	 */
-	public String classNameForDevice(String deviceName) {
-		return this.deviceProps.getProperty(deviceName);
+    /**
+     * Dump out all properties
+     */
+    public void printAll() {
+	for (Iterator i = this.deviceNames.iterator(); i.hasNext();) {
+	    String deviceName = (String) i.next();
+	    String deviceClass = this.deviceProps.getProperty(deviceName);
+	    System.out.println(deviceName + ":" + deviceClass);
 	}
-        
-        public Device classForIDString(String IDString)
-        {
-            Device device=null;
-            String deviceName=this.deviceIDProps.getProperty(IDString);
-           return this.classForDevice(deviceName);
-	}   
-
-	/**
-	 * Given a device name, return an instance of its class
-	 * @param deviceName the name of the device
-	 * @return an instance of the device's class
-	 */
-	public Device classForDevice(String deviceName) {
-		Device device = null;
-		String className = this.deviceProps.getProperty(deviceName);
-		try {
-			Class deviceClass = Class.forName(className);
-			device = (Device)deviceClass.newInstance();
-		}
-		catch (Exception e) {
-			ErrorMsg.reportError("Failed to find class for device",
-								 "Failed to find class for device '"
-								 + deviceName + "'");
-		}
-		return device;
-	}
-
-	/**
-	 * Dump out all properties
-	 */
-	public void printAll() {
-		for (Iterator i = this.deviceNames.iterator(); i.hasNext();)
-		{
-			String deviceName = (String)i.next();
-			String deviceClass = this.deviceProps.getProperty(deviceName);
-			System.out.println(deviceName + ":" + deviceClass);
-		}
-	}
-
+    }
 }
