ba7540955887fe6a288c8e7a0cd5ef6d9c179499
hayashi <hayashi@5361db82-7b37-4a53-9f17-e17e3a93d091>
2004-05-15 14:28:03 +0000

    make SynthDirsFilter and SynthFileFiler inner classes oof DeviceListWriter.

--- JSynthLib/core/DeviceListWriter.java
+++ JSynthLib/core/DeviceListWriter.java
@@ -10,70 +10,98 @@ import java.io.*;
  *
  * @author  Gerrit Gehnen
  * @version $Id$
+ * @see DevicesConfig
  */
 public class DeviceListWriter {
-    
-    public static void  writeList() {
-        Enumeration enum;
-        Vector synthis;
-        Properties props;
-        
-        props=new java.util.Properties();
+
+    private static void writeList() {
+        Properties props = new java.util.Properties();
+
         FileOutputStream out;
         try {
-            out=new FileOutputStream(Constants.RESOURCE_NAME_DEVICES_CONFIG);
-        }
-        catch (Exception e) {
+            out = new FileOutputStream(Constants.RESOURCE_NAME_DEVICES_CONFIG);
+        } catch (Exception e) {
             e.printStackTrace();
             return;
         }
-        MyClassLoader loader;
-        File synthdevicesDir =new File("synthdrivers");
-        File synthDirs[]=synthdevicesDir.listFiles(new SynthDirsFilter());
-        synthis=new Vector();
-        
-        for (int i=0;i<synthDirs.length ;i++) // for all subdirectories = synthesizer models
-        {
-            File actSynthDir= new File("synthdrivers",synthDirs[i].getName());
-            String synthDevices[]=actSynthDir.list(new SynthFileFilter());
-            try {
-                loader=new MyClassLoader(actSynthDir.getPath());
-                for (int j=0;j<synthDevices.length ;j++) {
-                    // Remove the ".class" from the list of files
-                    synthDevices[j]=synthDevices[j].substring(0,synthDevices[j].indexOf('.'));
-                    try {
-                        
-                        Class deviceclass=loader.loadClass(synthDevices[j],true);
-                        System.out.println(deviceclass);
-                        Device dev=(Device)deviceclass.newInstance();
-                        
-                        String shortname=synthDevices[j].substring(0,synthDevices[j].lastIndexOf("Device"));
-                        
-                        String manufacturer= dev.getManufacturerName();
-          //              System.out.println("Field:Manufacturer for "+synthDevices[j]+" "+manufacturer);
-                        props.setProperty(Constants.PROP_PREFIX_MANUFACTURER+shortname, dev.getManufacturerName());
-                        props.setProperty(Constants.PROP_PREFIX_ID_STRING+shortname, dev.getInquiryID());
-                        props.setProperty(Constants.PROP_PREFIX_DEVICE_NAME+shortname, dev.getManufacturerName()+" "+dev.getModelName()+" Driver");
-                        props.setProperty(Constants.PROP_PREFIX_DEVICE_CLASS+shortname, deviceclass.getName());
-                    }
-                    catch (Exception e) {
-                        ErrorMsg.reportStatus(e);
-                        System.out.println("Exception with "+synthDevices[j]);
-                    }
-                }
-            } catch (Exception e) {
-                ErrorMsg.reportStatus(e);
-            }
-        }
-        try{
-            props.store(out,"Generated devicesfile");
+        File synthdevicesDir = new File("synthdrivers");
+	// select only directories
+        File[] synthDirs = synthdevicesDir.listFiles(new SynthDirsFilter());
+
+	// for all subdirectories = synthesizer models
+        for (int i = 0; i < synthDirs.length; i++) {
+	    // select *Device.class
+	    File actSynthDir = new File("synthdrivers", synthDirs[i].getName());
+	    String[] synthDevices = actSynthDir.list(new SynthFileFilter());
+	    try {
+		MyClassLoader loader = new MyClassLoader(actSynthDir.getPath());
+		// for each Device class
+		for (int j = 0; j < synthDevices.length; j++) {
+		    // get Device class name by removing the ".class" from the list of files
+		    String devName = synthDevices[j].substring(0, synthDevices[j].indexOf('.'));
+		    try {
+			setProperty(loader, props, devName);
+		    } catch (Exception e) {
+			ErrorMsg.reportStatus(e);
+			ErrorMsg.reportStatus("Exception with " + devName);
+		    }
+		}
+	    } catch (Exception e) {
+		ErrorMsg.reportStatus(e);
+	    }
+	}
+	// save into synthdrivers.properties
+        try {
+            props.store(out, "Generated devicesfile");
             out.close();
             System.out.println("Done!");
-        }
-        catch (Exception e) {e.printStackTrace();}
+        } catch (Exception e) {
+	    ErrorMsg.reportStatus(e);
+	}
     }
-    
-    
+
+    private static void setProperty(MyClassLoader loader, Properties props, String devName)
+	throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+	Class deviceclass = loader.loadClass(devName, true);
+	System.out.println(deviceclass);
+
+	Device dev = (Device) deviceclass.newInstance();
+	// shortname : delete "Device" at the tail of devName
+	String shortname = devName.substring(0, devName.lastIndexOf("Device"));
+
+	props.setProperty(Constants.PROP_PREFIX_DEVICE_CLASS + shortname,
+			  deviceclass.getName());
+	props.setProperty(Constants.PROP_PREFIX_MANUFACTURER + shortname,
+			  dev.getManufacturerName());
+	props.setProperty(Constants.PROP_PREFIX_ID_STRING + shortname,
+			  dev.getInquiryID());
+	// saving only model name is better.
+	props.setProperty(Constants.PROP_PREFIX_DEVICE_NAME + shortname,
+			  dev.getManufacturerName() + " " + dev.getModelName() + " Driver");
+    }
+
+    /** FilenameFilter which select <code>*Device.class</code>. */
+    static class SynthFileFilter implements FilenameFilter {
+	public SynthFileFilter() {
+	}
+
+	public boolean accept (File dir, String name) {
+// 	    return ((name.endsWith ("Driver.class")||name.endsWith("Converter.class"))
+// 		    && name.indexOf ('$')==-1);
+	    return ((name.endsWith("Device.class")) && (name.indexOf('$') == -1));
+	}
+    }
+
+    /** FileFilter which select a directory. */
+    static class SynthDirsFilter implements FileFilter {
+	public SynthDirsFilter() {
+	}
+
+	public boolean accept(File dir) {
+	    return (dir.isDirectory());
+	}
+    }
+
     /**
      * @param args the command line arguments
      */
@@ -81,7 +109,4 @@ public class DeviceListWriter {
         DeviceListWriter.writeList();
         System.exit(0);
     }
-    
-    
-    
 }
--- JSynthLib/core/RecognizerEnumeration.java
+++ JSynthLib/core/RecognizerEnumeration.java
@@ -11,77 +11,63 @@ import java.util.Enumeration;
 import java.util.Vector;
 
 /**
- *
+ * Replaced by DeviceListWriter and DevicesConfig.
  * @author Gerrit Gehnen <Gerrit.Gehnen@gmx.de>
  * @version 0.2
  */
-public class RecognizerEnumeration extends Object
-{
+public class RecognizerEnumeration extends Object {
     static  Vector synthis;
     private Enumeration enum;
-    
-    static
-    {
+
+    static {
         MyClassLoader loader;
         File synthdevicesDir =new File ("synthdrivers");
-        File synthDirs[]=synthdevicesDir.listFiles (new SynthDirsFilter ());
+        File synthDirs[]=synthdevicesDir.listFiles (new DeviceListWriter.SynthDirsFilter ());
         synthis=new Vector ();
-        
-        for (int i=0;i<synthDirs.length ;i++) // for all subdirectories = synthesizer models
-        {
-            File actSynthDir= new File ("synthdrivers",synthDirs[i].getName ());
-            String synthDevices[]=actSynthDir.list (new SynthFileFilter ());
-            try
-            {
-                loader=new MyClassLoader (actSynthDir.getPath ());
-                for (int j=0;j<synthDevices.length ;j++)
-                {
-                    // Remove the ".class" from the list of files
-                    synthDevices[j]=synthDevices[j].substring (0,synthDevices[j].indexOf ('.'));
-                    try
-                    {
-                        // Relict of the Pre-Device era
-                     /*
-                   if (synthDevives[j].indexOf ("Converter")>0)
-                      synthis.add (0,(loader.loadClass
-                         (synthDrivers[j],true)).newInstance ());
-                      else
-                      */
-                        synthis.add ((loader.loadClass (synthDevices[j],true)).newInstance ());
-                        // System.out.println ("Synth Added "+synthDrivers[j]);
-                    }
-                    catch (Exception e)
-                    {
-                        ErrorMsg.reportStatus (e);
-                    }
-                }
-            } catch (Exception e)
-            {
-                ErrorMsg.reportStatus (e);
-            }
-        }
+
+        for (int i=0;i<synthDirs.length ;i++) { // for all subdirectories = synthesizer models
+	    File actSynthDir= new File ("synthdrivers",synthDirs[i].getName ());
+	    String synthDevices[]=actSynthDir.list (new DeviceListWriter.SynthFileFilter ());
+	    try {
+		loader=new MyClassLoader (actSynthDir.getPath ());
+		for (int j=0;j<synthDevices.length ;j++) {
+		    // Remove the ".class" from the list of files
+		    synthDevices[j]=synthDevices[j].substring (0,synthDevices[j].indexOf ('.'));
+		    try {
+			// Relict of the Pre-Device era
+			/*
+			  if (synthDevives[j].indexOf ("Converter")>0)
+			  synthis.add (0,(loader.loadClass
+			  (synthDrivers[j],true)).newInstance ());
+			  else
+			*/
+			synthis.add ((loader.loadClass (synthDevices[j],true)).newInstance ());
+			// System.out.println ("Synth Added "+synthDrivers[j]);
+		    } catch (Exception e) {
+			ErrorMsg.reportStatus (e);
+		    }
+		}
+	    } catch (Exception e) {
+		ErrorMsg.reportStatus (e);
+	    }
+	}
     }
-    
+
     /** Creates new RecognizerVector */
-    public RecognizerEnumeration ()
-    {
+    public RecognizerEnumeration () {
         enum=synthis.elements ();
     }
-    /** Resets the Enumeration-Pointer to the first element
-     */
-    public void reset ()
-    {
-        
+
+    /** Resets the Enumeration-Pointer to the first element */
+    public void reset () {
         enum=synthis.elements ();
     }
-    public boolean hasMoreElements ()
-    {
+
+    public boolean hasMoreElements () {
         return enum.hasMoreElements ();
     }
-    
-    public Device nextElement ()
-    {
+
+    public Device nextElement () {
         return (Device) enum.nextElement ();
     }
-    
 }
