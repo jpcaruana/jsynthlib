8d7dcb62cd0f48b21189f83d22d647a3a5538e9a
ttittmann <ttittmann@5361db82-7b37-4a53-9f17-e17e3a93d091>
2004-06-26 10:22:58 +0000

    added license.txt file and changed build.sh script

--- JSynthLib/midiprovider/LinuxCharDev/build.sh
+++ JSynthLib/midiprovider/LinuxCharDev/build.sh
@@ -2,5 +2,7 @@
 echo Compiling...
 for n in LinuxCharDevMidiProvider/*.java; do javac -g $n; done
 echo Building jar...
-jar cf ../LinuxCharDev.jar META-INF/services/javax.sound.midi.spi.MidiDeviceProvider LinuxCharDevMidiProvider/*.class
+# put all files into the jar package not only the class files
+#jar cf ../LinuxCharDev.jar META-INF/services/javax.sound.midi.spi.MidiDeviceProvider LinuxCharDevMidiProvider/*.class
+jar cf ../LinuxCharDev.jar .
 echo Done.
