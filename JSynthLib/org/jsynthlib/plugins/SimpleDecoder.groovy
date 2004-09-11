fc840528d068ec76030aff8d22db3e4ceb4a5f1c
ribrdb <ribrdb@5361db82-7b37-4a53-9f17-e17e3a93d091>
2004-09-11 19:32:05 +0000

    Fix typo, impropper usage of properties.

--- JSynthLib/org/jsynthlib/plugins/SimpleDecoder.groovy
+++ JSynthLib/org/jsynthlib/plugins/SimpleDecoder.groovy
@@ -25,11 +25,11 @@ class SimpleDecoder extends Decoder {
     public SimpleDecoder(Integer size, Boolean be) {
         word_size = size
         big_endian = be
-        size = 0
+        this.size = 0
         default_size = word_size < 7 ? 2 : 1
     }
     
-    public XMLParameter newParameter(String type) {
+    public XMLParameter newParameter(String type) { 
         Parameter p = new Parameter(this, size)
         p.setSize(default_size) // Set default size
         t = 0
@@ -70,7 +70,7 @@ class SimpleDecoder extends Decoder {
     private int _decode(int offset, int size, boolean signed, byte[] msg) {
         Integer shift, offset
         if (big_endian) {
-            shift = word_size*(param.size - 1)
+            shift = word_size*(param.getSize() - 1)
             offset = -word_size
         } else {
             shift = 0
@@ -88,26 +88,27 @@ class SimpleDecoder extends Decoder {
     // XXX: Add support for a mapping table for weird encodings.
     public String decodeString(XMLParameter p, byte[] msg) {
         if (p.size == 1) {
-            return new String(msg, p.offset, p.length, p.charset)
+            return new String(msg, p.getOffset(), p.getLength(), p.getCharset())
         } else {
+        		o = p.getOffset()
+        		s = p.getSize()
             StringBuffer b = new StringBuffer()
             for (i in 0 .. p.length - 1) {
-                b.append((char)_decode(p.offset + p.size*i, p.size, msg))
+                b.append((char)_decode(o + s*i, s, msg))
             }
         }
     }
 
     public void encode(int value, XMLParameter param, byte[] msg) {
-        b = _encode(value - param.getBase(), param.size)
-
-        for (i in 0 .. param.size - 1)
-            msg[i + param.offset] = b[i]
+        b = _encode(value - param.getBase(), param.getSize())
+        for (i in 0 .. param.getSize() - 1)
+            msg[i + param.getOffset()] = b[i]
     }
     
     private Byte[] _encode(int value, int size) {
-        Integer shift, offset
-        Byte[] b = new Byte[p.size]
-        mask = 1 << (word+size + 1)
+        shift = offset = 0
+        Byte[] b = new Byte[size]
+        mask = 1 << (word_size + 1)
         if (big_endian) {
             shift = 0
             offset = word_size
@@ -124,8 +125,8 @@ class SimpleDecoder extends Decoder {
     
     public String encode(int value, XMLParameter param) {
         retval = new StringBuffer()
-        b = _encode(value + param.getBase(), param.size)
-        for (i in 0 .. param.size - 1) {
+        b = _encode(value + param.getBase(), param.getSize())
+        for (i in 0 .. param.getSize() - 1) {
             if (b[i] < 0x10)
                 retval.append("0")
             retval.append(Integer.toHexString((int)b[i]))
@@ -135,16 +136,18 @@ class SimpleDecoder extends Decoder {
 
     public void encodeString(String value, XMLParameter p, byte[] msg) {
         i = 0
+        size = p.getSize()
+        offset = p.getOffset()
         while (i < p.length && i < value.length() ) {
-            b = _encode(value.charAt(i), p.size)
-            for (j in 0 .. p.size - 1)
-                msg[p.offset + i*p.size + j] = b[i]
+            b = _encode(value.charAt(i), size)
+            for (j in 0 .. size - 1)
+                msg[offset + i*size + j] = b[i]
             i += 1
         }
-        while (i < p.length) {
-            b = _encode(32, p.size) // " "
-            for (j in 0 .. p.size - 1)
-                msg[p.offset + i*p.size + j] = b[i]
+        while (i < p.getLength()) {
+            b = _encode(32, size) // " "
+            for (j in 0 .. size - 1)
+                msg[offset + i*size + j] = b[i]
             i += 1        
         }
     }
@@ -165,7 +168,7 @@ class SimpleDecoder extends Decoder {
 	
 }
 class Parameter extends XMLParameter {
-	protected int offset;
+	public int offset;
 	protected int size;
 	protected String charset
 	protected boolean signed = false
