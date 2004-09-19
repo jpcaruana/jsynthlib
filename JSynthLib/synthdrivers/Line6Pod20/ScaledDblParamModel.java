dabe56d86ff32e4aca86d98b5491c8dc2e5f082d
jwweber55434 <jwweber55434@5361db82-7b37-4a53-9f17-e17e3a93d091>
2004-09-19 18:44:05 +0000

    Added javadoc comments. Fixed problem handling negative numbers in low-order byte.

--- JSynthLib/synthdrivers/Line6Pod20/ScaledDblParamModel.java
+++ JSynthLib/synthdrivers/Line6Pod20/ScaledDblParamModel.java
@@ -23,22 +23,67 @@ package synthdrivers.Line6Pod20;
 
 import core.*;
 
-/** Scalable Double ParamModel--to allow different max values for CC and Sysex -- for two byte sysex value*/
+/** Scalable Double (two-byte) ParamModel--Used for two-byte sysex parameters.
+* Also allows different max values for CC and Sysex.
+*
+* @author Jeff Weber
+*/
 class ScaledDblParamModel extends ParamModel {
+    /** The mminimum CC value that will be used for the control. This is used
+    * when calculating a scaling factor. The scaling factor is calculated as 
+    * (maxSysex - minSysex) / (maxCC - minCC).
+    */
     private int minCC = 0;
+
+    /** The minimum CC value that will be used for the control. This is used
+    * when calculating a scaling factor. The scaling factor is calculated as 
+    * (maxSysex - minSysex) / (maxCC - minCC).
+    */
     private int maxCC;
+
+    /** The mminimum CC value that will be used for the control. This is used
+        * when calculating a scaling factor. The scaling factor is calculated as 
+        * (maxSysex - minSysex) / (maxCC - minCC).
+        */
     private int minSysex = 0;
+
+    /** The mminimum CC value that will be used for the control. This is used
+        * when calculating a scaling factor. The scaling factor is calculated as 
+        * (maxSysex - minSysex) / (maxCC - minCC).
+        */
     private int maxSysex;
+
+    /** Reverses the range of values sent by the sysexWidget over the range
+        * max to min. A value of true causes the range to be reversed.
+        */
     private boolean reverse = false;
     
+    /** Constructs a ScaledDblParamModel. Patch p is the reference to the patch
+        * containing the sysex record. int o is the offset into the sysex record
+        * in non-nibblized bytes, not including the header bytes. */
     ScaledDblParamModel(Patch p, int o) {
         this(p, o, 0, 1, 0, 1, false);
     }
     
+    /** Constructs a ScaledDblParamModel. This is used to scale values when the
+        * max sysex value is different than the max CC value. Patch p is the
+        * reference to the patch containing the sysex record. int o is the offset
+        * into the sysex record in non-nibblized bytes, not including the header
+        * bytes. int maxCC is the maximum CC value. int maxSysex is the max sysex
+        * value*/
     ScaledDblParamModel(Patch p, int o, int maxCC, int maxSysex) {
         this(p, o, 0, maxCC, 0, maxSysex, false);
     }
     
+    /** Constructs a ScaledDblParamModel. This is used to scale values when the
+        * max sysex value is different than the max CC value and the minimum CC
+        * and sysex values are not zero. Patch p is the reference to the patch
+        * containing the sysex record. int o is the offset into the sysex record
+        * in non-nibblized bytes, not including the header bytes. int minCC and
+        * int maxCC are the the minimum and maximum CC values, respectively. int
+        * minSysex and int maxSysex are the minimum and maximum sysex values, 
+        * respectively. boolean reverse reverses the range to min to max when
+        * set to true.*/
     ScaledDblParamModel(Patch p, int o, int minCC, int maxCC, int minSysex, int maxSysex, boolean reverse) {
         super(p, o);
         this.reverse = reverse;
@@ -48,6 +93,9 @@ class ScaledDblParamModel extends ParamModel {
         this.maxSysex = maxSysex;
     }
     
+    /** Scales the value of i according maxSysex and sets the sysex value at
+        * offset ofs to the result. Also reverses the value over the range if
+        * reverse is true. */
     public void set(int i) {
         if (reverse) {
             i = (maxCC - minCC) - (i - minCC) + minCC;
@@ -59,11 +107,11 @@ class ScaledDblParamModel extends ParamModel {
         PatchBytes.setSysexByte(patch, 9, ofs+1, lsB);
     }
     
+    /** Returns the value at offset ofs, scaled according to maxCC and maxSysex.
+        Also reverses the value over the range if reverse is true. */
     public int get() {
-        int msB = (int)PatchBytes.getSysexByte(patch.sysex, 9, ofs);
-        int lsB = (int)PatchBytes.getSysexByte(patch.sysex, 9, ofs+1);
-        System.out.println("msB = " + msB);  //Test
-        System.out.println("lsB = " + lsB);  //Test
+        int msB = 0xFF & (int)PatchBytes.getSysexByte(patch.sysex, 9, ofs);
+        int lsB = 0xFF & (int)PatchBytes.getSysexByte(patch.sysex, 9, ofs+1);
 
         int returnValue = msB << 8;
         returnValue = returnValue | lsB;
