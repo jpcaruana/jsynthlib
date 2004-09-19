dd0e3e8832d8fdf80558069486b9bd19845ef0fa
jwweber55434 <jwweber55434@5361db82-7b37-4a53-9f17-e17e3a93d091>
2004-09-19 18:45:23 +0000

    Added javadoc comments. Removed reverse range functionality (not used for delay speed parameter).

--- JSynthLib/synthdrivers/Line6Pod20/DelayCoarseSpeedModel.java
+++ JSynthLib/synthdrivers/Line6Pod20/DelayCoarseSpeedModel.java
@@ -23,27 +23,71 @@ package synthdrivers.Line6Pod20;
 
 import core.*;
 
-/** For the coarse speed, we're only interested in looking at bytes 27-28. So to get the coarse value we just
-do a mod 3 (or mod 0x03) instead of mod 768 (or mod 0x0300) and subtract that result from the original value.*/
+/** Delay Speed is represented by three bytes in offsets 27-29. There are two
+* controls in the editor, one for coarse adjustment and the other for fine
+* adjustment. The range for coarse sysex values is 0-97536, where the max value
+* is represented in bytes 27-29 as 0x017D00. The range for fine sysex values is 
+* 0-762, where the max value is represented in bytes 27-29 as 0x0002FA. The value
+* stored in offset 27-29 of the sysex record is simply the sum of the course and
+* fine values. So the value with the coarse and fine values both set to maximum
+* is 98298 or 0x017FFA. For the coarse control the control range of 0-127 split
+* over the range 0-97536 gives steps of 768 or 0x300. The two values are 
+* separated as follows:
+* <br />
+* <code>
+*     Coarse Value = Combined Value mod 300
+* <br />
+*     Fine Value = Combined Value - Coarse Value
+* </code>
+*
+* @author Jeff Weber
+*/
 class DelayCoarseSpeedModel extends ParamModel {
+    /** The minimum CC value that will be used for the control. This is used
+    * when calculating a scaling factor. The scaling factor is calculated as 
+    * (maxSysex - minSysex) / (maxCC - minCC).
+    */
     private int minCC = 0;
+
+    /** The maximum CC value that will be used for the control. This is used
+    * when calculating a scaling factor. The scaling factor is calculated as 
+    * (maxSysex - minSysex) / (maxCC - minCC).
+    */
     private int maxCC;
+
+    /** The minimum CC value that will be used for the control. This is used
+        * when calculating a scaling factor. The scaling factor is calculated as 
+        * (maxSysex - minSysex) / (maxCC - minCC).
+        */
     private int minSysex = 0;
+
+    /** The maximum CC value that will be used for the control. This is used
+        * when calculating a scaling factor. The scaling factor is calculated as 
+        * (maxSysex - minSysex) / (maxCC - minCC).
+        */
     private int maxSysex;
-    private boolean reverse = false;
+
+    /** The scaling factor, calculated as 
+        * (maxSysex - minSysex) / (maxCC - minCC).
+        */
     private int scaleFactor;
     
-    DelayCoarseSpeedModel(Patch p, int o) {
-        this(p, o, 0, 1, 0, 1, false);
-    }
-    
+    /** Constructs a DelayCoarseSpeedModel. Patch p is the reference to the patch
+        * containing the sysex record. int o is the offset into the sysex record
+        * in non-nibblized bytes, not including the header bytes. int maxCC is 
+        * the maximum CC value. int maxSysex is the max sysex value */
     DelayCoarseSpeedModel(Patch p, int o, int maxCC, int maxSysex) {
-        this(p, o, 0, maxCC, 0, maxSysex, false);
+        this(p, o, 0, maxCC, 0, maxSysex);
     }
     
-    DelayCoarseSpeedModel(Patch p, int o, int minCC, int maxCC, int minSysex, int maxSysex, boolean reverse) {
+    /** Constructs a DelayCoarseSpeedModel. Patch p is the reference to the patch
+        * containing the sysex record. int o is the offset into the sysex record
+        * in non-nibblized bytes, not including the header bytes. int min CC and
+        * int maxCC are the minimum and maximum CC values, respectively. int 
+        * minSysex and int maxSysex are the minimum and maximum sysex values, 
+        * respectively */
+    private DelayCoarseSpeedModel(Patch p, int o, int minCC, int maxCC, int minSysex, int maxSysex) {
         super(p, o);
-        this.reverse = reverse;
         this.minCC = minCC;
         this.maxCC = maxCC;
         this.minSysex = minSysex;
@@ -51,10 +95,9 @@ class DelayCoarseSpeedModel extends ParamModel {
         this.scaleFactor = maxSysex / maxCC;
     }
     
+    /** Scales the value of i according scaleFactor and sets the sysex value at
+        * offset ofs to the result. */
     public void set(int i) {
-        if (reverse) {
-            i = (maxCC - minCC) - (i - minCC) + minCC;
-        }
         int sysexValue = ((i - minCC) * (maxSysex - minSysex) / (maxCC - minCC)) + minSysex;
         sysexValue += getFineValue();
         byte msB = (byte)(sysexValue / 256);
@@ -63,15 +106,17 @@ class DelayCoarseSpeedModel extends ParamModel {
         PatchBytes.setSysexByte(patch, 9, ofs+1, lsB);
     }
     
+    /** Returns the value at offset ofs, scaled according to scaleFactor.
+        */
     public int get() {
         int returnValue = getCoarseValue();
         returnValue = ((returnValue - minSysex) * (maxCC - minCC) / (maxSysex - minSysex)) + minCC;
-        if (reverse) {
-            returnValue = (maxCC - minCC) - (returnValue - minCC) + minCC;
-        }
         return returnValue;
     }
     
+    /** Returns the value of the two bytes representing the delay speed as an
+        * int. The value returned is the composite value representing both the
+        * coarse speed and the fine speed.*/
     private int getBytesValue() {
         int msB = (int)PatchBytes.getSysexByte(patch.sysex, 9, ofs);
         int lsB = (int)PatchBytes.getSysexByte(patch.sysex, 9, ofs+1);
@@ -80,11 +125,13 @@ class DelayCoarseSpeedModel extends ParamModel {
         return bytesValue;
     }
     
+    /** Returns the coarse speed value.*/
     private int getCoarseValue() {
         int coarseValue = getBytesValue() - getFineValue();
         return coarseValue;
     }
     
+    /** Returns the fine speed value.*/
     private int getFineValue() {
         int fineValue = getBytesValue() % scaleFactor;
         return fineValue;
--- JSynthLib/synthdrivers/Line6Pod20/DelayFineSpeedModel.java
+++ JSynthLib/synthdrivers/Line6Pod20/DelayFineSpeedModel.java
@@ -23,28 +23,50 @@ package synthdrivers.Line6Pod20;
 
 import core.*;
 
-/** Delay Speed is represented by three bytes in offsets 27-29. There are two controls in the editor, one for coarse 
-adjustment and the other for fine adjustment. The range for coarse values in the sysex record is 0-97536, where 
-the max value is represented in bytes 27-29 as 0x017D00. The range for fine values in the sysex record is 0-762, 
-where the max value is represented in bytes 27-29 as 0x0002FA. The value stored in offset 27-29 of the sysex 
-record is simply the sum of the course and fine values. So the value with the coarse and fine values both set to 
-maximum is 98298 or 0x017FFA. For the coarse control the control range of 0-127 split over the range 0-97536 gives 
-us steps of 768 or 0x300. So to separate the two value, we can take the mod 300 of the combined value to obtain 
-the coarse value. Subtracting this from the original value gives us the fine value.*/
+/** Delay Speed is represented by three bytes in offsets 27-29. There are two
+* controls in the editor, one for coarse adjustment and the other for fine
+* adjustment. The range for coarse sysex values is 0-97536, where the max value
+* is represented in bytes 27-29 as 0x017D00. The range for fine sysex values is 
+* 0-762, where the max value is represented in bytes 27-29 as 0x0002FA. The value
+* stored in offset 27-29 of the sysex record is simply the sum of the course and
+* fine values. So the value with the coarse and fine values both set to maximum
+* is 98298 or 0x017FFA. For the coarse control the control range of 0-127 split
+* over the range 0-97536 gives steps of 768 or 0x300. The two values are 
+* separated as follows:
+* <br />
+* <code>
+*     Coarse Value = Combined Value mod 300
+* <br />
+*     Fine Value = Combined Value - Coarse Value
+* </code>
+*
+* @author Jeff Weber
+*/
 class DelayFineSpeedModel extends ParamModel {
+    /** The maximum CC value that will be used for the control. This is used
+    * when calculating a scaling factor. The scaling factor is calculated as 
+    * maxSysex / maxCC.
+    */
     private int maxCC;
+
+    /** The maximum CC value that will be used for the control. This is used
+    * when calculating a scaling factor. The scaling factor is calculated as 
+    * maxSysex / maxCC.
+    */
     private int maxSysex;
     
-    DelayFineSpeedModel(Patch p, int o) {
-        this(p, o, 1, 1);
-    }
-    
+    /** Constructs a DelayFineSpeedModel. Patch p is the reference to the patch
+        * containing the sysex record. int o is the offset into the sysex record
+        * in non-nibblized bytes, not including the header bytes. int maxCC is 
+        * the maximum CC value. int maxSysex is the max sysex value */
     DelayFineSpeedModel(Patch p, int o, int maxCC, int maxSysex) {
         super(p, o);
         this.maxCC = maxCC;
         this.maxSysex = maxSysex;
     }
     
+    /** Scales the value of i according maxSysex and sets the sysex value at
+        * offset ofs to the result. */
     public void set(int i) {
         int coarseValue = getCoarseValue();
         int newValue = coarseValue + (i * maxSysex / maxCC);
@@ -59,14 +81,20 @@ class DelayFineSpeedModel extends ParamModel {
         PatchBytes.setSysexByte(patch, 9, ofs, byte3);
     }
     
+    /** Returns the value at offset ofs, scaled according to maxCC and maxSysex.
+        */
     public int get() {
         int returnValue =  getFineValue() * maxCC / maxSysex;
         return returnValue;
     }
+
+    /** Returns the value of the three bytes representing the delay speed as an
+        * int. The value returned is the composite value representing both the
+        * coarse speed and the fine speed.*/
     private int getBytesValue() {
-        int msB  = (int)(0xff & PatchBytes.getSysexByte(patch.sysex, 9, ofs-2));
-        int midB = (int)(0xff & PatchBytes.getSysexByte(patch.sysex, 9, ofs-1));
-        int lsB  = (int)(0xff & PatchBytes.getSysexByte(patch.sysex, 9, ofs));
+        int msB  = (int)(0xFF & PatchBytes.getSysexByte(patch.sysex, 9, ofs-2));
+        int midB = (int)(0xFF & PatchBytes.getSysexByte(patch.sysex, 9, ofs-1));
+        int lsB  = (int)(0xFF & PatchBytes.getSysexByte(patch.sysex, 9, ofs));
         
         int bytesValue = msB << 16;
         bytesValue = bytesValue | midB << 8;
@@ -74,11 +102,13 @@ class DelayFineSpeedModel extends ParamModel {
         return bytesValue;
     }
     
+    /** Returns the coarse speed value.*/
     private int getCoarseValue() {
         int coarseValue = getBytesValue() - getFineValue();
         return coarseValue;
     }
     
+    /** Returns the fine speed value.*/
     private int getFineValue() {
         int fineValue = getBytesValue() % (3 * 256);
         return fineValue;
