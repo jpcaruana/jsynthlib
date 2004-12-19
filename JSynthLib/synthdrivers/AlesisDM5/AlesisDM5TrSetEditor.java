dd69ea16160eea69ccd71e0b7d52f5e2e2abe707
jwweber55434 <jwweber55434@5361db82-7b37-4a53-9f17-e17e3a93d091>
2004-12-19 04:16:15 +0000

    Changed name of class DM5Sender to TrigSender.

--- JSynthLib/synthdrivers/AlesisDM5/AlesisDM5TrSetEditor.java
+++ JSynthLib/synthdrivers/AlesisDM5/AlesisDM5TrSetEditor.java
@@ -26,10 +26,6 @@ import javax.swing.*;
 import java.awt.*;
 import javax.swing.border.*;
 
-// packages needed for class DM5Sender
-import javax.sound.midi.ShortMessage;
-import javax.sound.midi.InvalidMidiDataException;
-
 /** Alesis DM5 Trigger Setup Editor
 * 
 * @author Jeff Weber
@@ -68,7 +64,7 @@ public class AlesisDM5TrSetEditor extends PatchEditorFrame {
         addWidget(panel,
                   new KnobWidget("Gain", patch, 0, 99, 0,
                                  new ParamModel(patch, headerSize + trigNum),
-                                 new DM5Sender(trigNum, DM5Sender.TR_GAIN, 99)),
+                                 new TrigSender(trigNum, TrigSender.TR_GAIN, 99)),
                   0, 0,
                   1, 1,
                   fdrNbrBase+1);
@@ -77,7 +73,7 @@ public class AlesisDM5TrSetEditor extends PatchEditorFrame {
                   new KnobWidget("Velocy",
                                  patch, 0, 7, 0,
                                  new ParamModel(patch, 19 + (trigNum * 4)),
-                                 new DM5Sender(trigNum, DM5Sender.TR_V_CURVE, 7)),
+                                 new TrigSender(trigNum, TrigSender.TR_V_CURVE, 7)),
                   1, 0,
                   1, 3,
                   fdrNbrBase+2);
@@ -86,7 +82,7 @@ public class AlesisDM5TrSetEditor extends PatchEditorFrame {
                   new KnobWidget("X-Talk",
                                  patch, 0, 99, 0,
                                  new ParamModel(patch, 20 + (trigNum * 4)),
-                                 new DM5Sender(trigNum, DM5Sender.TR_X_TALK, 99)),
+                                 new TrigSender(trigNum, TrigSender.TR_X_TALK, 99)),
                   2, 0,
                   1, 3,
                   fdrNbrBase+3);
@@ -95,7 +91,7 @@ public class AlesisDM5TrSetEditor extends PatchEditorFrame {
                   new KnobWidget("Noise Flr",
                                  patch, 0, 99, 0,
                                  new ParamModel(patch, 21 + (trigNum * 4)),
-                                 new DM5Sender(trigNum, DM5Sender.TR_NOISE_FLR, 99)),
+                                 new TrigSender(trigNum, TrigSender.TR_NOISE_FLR, 99)),
                   3, 0,
                   1, 3,
                   fdrNbrBase+4);
@@ -104,65 +100,9 @@ public class AlesisDM5TrSetEditor extends PatchEditorFrame {
                   new KnobWidget("Decay",
                                  patch, 0, 99, 0,
                                  new ParamModel(patch, 22 + (trigNum * 4)),
-                                 new DM5Sender(trigNum, DM5Sender.TR_DECAY, 99)),
+                                 new TrigSender(trigNum, TrigSender.TR_DECAY, 99)),
                   4, 0,
                   1, 3,
                   fdrNbrBase+5);
-    }
-}
-
-class DM5Sender implements SysexWidget.ISender {
-    final static int TR_V_CURVE        = 1;
-    final static int TR_NOTE_NBR       = 2;
-    final static int TR_GAIN           = 3;
-    final static int TR_X_TALK         = 4;
-    final static int TR_DECAY          = 5;
-    final static int TR_NOISE_FLR      = 6;
-    
-    private final static int NRPN_MSB          = 99;
-    private final static int NRPN_LSB          = 98;
-    private final static int DATA_ENTRY_MSB    = 6;
-    private final static int SELECT_ACTIVE_TRG = 0;
-    
-    private final static int MAX_TRIG_NUM = 11;
-    private static int LAST_TRIG_NUM = 99;
-    private int param;
-    private int trigNum;
-    private int max;
-    
-    public DM5Sender(int trigNum, int param, int max) {
-        this.trigNum = trigNum;
-        this.param = param;
-        this.max = max;
-    }
-    
-    public void send(IPatchDriver driver, int value) {
-        if (trigNum != LAST_TRIG_NUM) {
-            try {
-                driver.send(newControlChange(driver, NRPN_MSB, 0));  // Send NRPN MSB
-                driver.send(newControlChange(driver, NRPN_LSB, SELECT_ACTIVE_TRG));  // Command to select active trigger number (NRPN LSB)
-                driver.send(newControlChange(driver, 6, trigNum * 127 / MAX_TRIG_NUM));  // Set the Trigger Number
-            } catch  (InvalidMidiDataException e) {
-                ErrorMsg.reportStatus(e);
-            }
-            LAST_TRIG_NUM = trigNum;            
-
-            try {
-                Thread.sleep (50);
-            } catch (Exception e) {}
-        }
-
-        try {
-            driver.send(newControlChange(driver, NRPN_LSB, param));  // Command to select the parameter
-            driver.send(newControlChange(driver, DATA_ENTRY_MSB, value * 127 / max));  // Set the value of the parameter
-        } catch (InvalidMidiDataException e) {
-            ErrorMsg.reportStatus(e);
-        }
-    }
-    
-    private ShortMessage newControlChange(IPatchDriver driver, int controlNumber, int value) throws InvalidMidiDataException {
-        ShortMessage ccMessage = new ShortMessage();
-        ccMessage.setMessage(ShortMessage.CONTROL_CHANGE, driver.getDevice().getChannel() - 1, controlNumber, value);
-        return ccMessage;
     }
 }
