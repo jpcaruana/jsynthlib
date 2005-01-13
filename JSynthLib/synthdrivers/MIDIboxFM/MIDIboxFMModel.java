74dc2699a8e29d45d476732919149ab35e323466
(no author) <(no author)@5361db82-7b37-4a53-9f17-e17e3a93d091>
2005-01-13 21:35:09 +0000

    This commit was manufactured by cvs2svn to create tag 'start'.

---
+++ JSynthLib/synthdrivers/MIDIboxFM/MIDIboxFMModel.java
@@ -0,0 +1,102 @@
+/*
+ * JSynthlib Parameter Model for MIDIbox FM
+ * =====================================================================
+ * @author  Thorsten Klose
+ * @version $Id$
+ *
+ * Copyright (C) 2005  Thorsten.Klose@gmx.de   
+ *                     http://www.uCApps.de
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package synthdrivers.MIDIboxFM;
+
+import core.ParamModel;
+import core.Patch;
+
+class MIDIboxFMModel extends ParamModel
+{
+    int flag;
+    int bitmask;
+    int []mapped_values;
+
+    public MIDIboxFMModel(Patch _patch, int _offset)
+    {
+	ofs = _offset + 10;
+	patch = _patch;
+	flag = -1;
+    }
+
+    public MIDIboxFMModel(Patch _patch, int _offset, int _flag)
+    {
+	ofs     = _offset + 10;
+	patch   = _patch;
+	flag    = _flag;
+	bitmask = (1 << flag);
+	mapped_values = new int[]{}; // (empty)
+    }
+
+    public MIDIboxFMModel(Patch _patch, int _offset, int _flag, int _bitmask)
+    {
+	int i;
+
+	ofs     = _offset + 10;
+	patch   = _patch;
+	flag    = _flag;
+	bitmask = _bitmask << flag;
+	mapped_values = new int[]{}; // (empty)
+    }
+
+    public MIDIboxFMModel(Patch _patch, int _offset, int _flag, int _bitmask, int []_mapped_values)
+    {
+	ofs     = _offset + 10;
+	patch   = _patch;
+	flag    = _flag;
+	bitmask = _bitmask << flag;
+	mapped_values = _mapped_values;
+    }
+
+    public void set(int i)
+    {
+	if( flag == -1 ) {
+	    patch.sysex[ofs] = (byte)i;
+	} else {
+	    patch.sysex[ofs]=(byte)(patch.sysex[ofs]&(~bitmask));
+	    if( mapped_values.length > 0 )
+		patch.sysex[ofs] |= (byte)mapped_values[i];
+	    else
+		patch.sysex[ofs] |= (byte)i << flag;
+	}
+    }
+
+    public int get()
+    {
+	if( flag == -1 )
+	    return patch.sysex[ofs];
+	else {
+	    if( mapped_values.length > 0 ) {
+		int value;
+
+		value = (patch.sysex[ofs] & bitmask) >> flag;
+		for(int i=0; i<mapped_values.length; ++i)
+		    if( mapped_values[i] == value )
+			return i;
+		return 0;
+	    } else
+		return (patch.sysex[ofs] & bitmask) >> flag;
+	}
+    }
+}
