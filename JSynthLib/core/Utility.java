d757b0d38142afa0c5321ef92b31a137dec6198a
jemenake <jemenake@5361db82-7b37-4a53-9f17-e17e3a93d091>
2006-02-06 23:47:49 +0000

    Added some more ways to invoke hexDump()

--- JSynthLib/core/Utility.java
+++ JSynthLib/core/Utility.java
@@ -58,6 +58,137 @@ public class Utility extends Object {
     }
     //----- End phil@muqus.com
 
+
+    //----- Start Joe Emenaker
+    /**
+     * convert a byte array into a hexa-dump string, with or without spaces between the bytes.
+     *
+     * @param d a <code>byte[]</code> array to be converted.
+     * @param offset array index from which dump starts.
+     * @param len number of bytes to be dumped.  If -1, dumps to the
+     * end of the array.
+     * @param bytes number of bytes per line.  If equal or less than
+     * 0, no newlines are inserted.
+     * @param wantspaces whether or not to insert spaces between bytes
+     * @return hexa-dump string.
+     */
+    public static String hexDump(byte[] d, int offset, int len, int bytes, boolean wantspaces) {
+    	StringBuffer buf = new StringBuffer();
+	    if (len == -1 || offset + len > d.length)
+    	    len = d.length - offset;
+    	for (int i = 0; i < len; i++) {
+	        int c = (d[offset + i] & 0xff);
+	        if (c < 0x10)
+                buf.append("0");
+	        buf.append(Integer.toHexString(c));
+	        if (bytes > 0 && (i % bytes == bytes - 1 && i != len - 1))
+		        buf.append("\n");
+	        else
+                if (i != len - 1 && wantspaces)
+		            buf.append(" ");
+	    }
+	    return buf.toString();
+    }
+
+    public static String hexDump(byte[] d, int offset, int len, int bytes, int addressLength, boolean characters) {
+        String output = "";
+        if(offset > d.length) {
+            return("offset out of bounds of byte array");
+        }
+        if(offset + len > d.length) {
+            len = d.length - offset;
+        }
+
+        if(addressLength > 0) {
+            String addressstring = Integer.toHexString(offset);
+            while(addressstring.length() < addressLength) {
+                addressstring = "0" + addressstring;
+            }
+            output += addressstring + " - ";
+        }
+        String hexdigits = hexDump(d, offset, len, bytes, true);
+        // If hexdigits is too short, pad with spaces so that the last line lines up with the others.
+        if(characters) {
+            while(hexdigits.length() < 3*bytes-1) {
+                hexdigits += "   ";
+            }
+        }
+        output += hexdigits;
+        if(characters) {
+            output += " - ";
+            for(int i=offset; i<offset+len; i++) {
+                if(d[i] >= 32 && d[i] <= 126) {
+                    output += new Character((char)d[i]).toString();
+                } else {
+                    output += ".";
+                }
+            }
+        }
+        return(output);
+    }
+
+    /**
+     * convert a byte array into a string of hex values, optionally including their character
+     * equivalents and/or the address/offset of the beginning of the line. With character
+     * equivalents, unprintable characters are replaced by spaces. For example:
+     *     65 78 61 6D 70 6C 65 01 EF 31 38 F3 F1 - EXAMPLE..18..
+     *
+     * If <code>addresses</code> is true, then the hex representation of the offset will
+     * be put at the beginning of each line. For example:
+     *     013A0 - 65 78 61 6D 70 6C 65 01 - EXAMPLE.
+     *     013A8 - EF 31 38 F3 F1 65 64 63 - .18..EDC
+     *     013B0 - 38 34 36 37 38 31 37 37 - 84678177
+     *
+     * If <code>len</code> is positive non-zero, only <code>len</code> bytes will be
+     * represented on each line.
+     *
+     * @param d a <code>byte[]</code> array to be converted.
+     * @param offset array index from which dump starts.
+     * @param len number of bytes to be dumped.  If -1, dumps to the
+     * end of the array.
+     * @param bytes number of columns (bytes) to put in each line. If less than
+     * or equal to zero, no line-breaks are inserted.
+     * @return hexa-dump string.
+     */
+    public static String hexDump(byte[] d, int offset, int len, int bytes, boolean addresses, boolean characters) {
+        // Is offset beyond the end of array?
+        if(offset >= d.length) {
+            return("");
+        }
+        // Is offset+len beyond the end of the array?
+        if(offset+len >= d.length || len < 0) {
+            len = d.length - offset; // Set len to get remaining bytes
+        }
+        // If bytes <=0, set it to len so that we get the remaining bytes on one line
+        if(bytes <= 0) {
+            bytes = len;
+        }
+
+        // How many digits to use for address?
+        int addresslen = 0;
+        // Keep adding 2 digits until we can repesent the highest number we need
+        if(addresses) {
+            int maxnumber = 1;
+            while(maxnumber < offset+len) {
+                addresslen += 2;
+                maxnumber *= 256;
+            }
+        }
+
+        String output = "";
+        if(len <= bytes) {
+            output = hexDump(d, offset, len, bytes, addresslen, characters);
+        } else {
+            // The bytes won't fit. Split them up and call hexDump for each one
+            for(int i=0; i<len; i+=bytes) {
+                output = output + hexDump(d, offset+i, bytes, bytes, addresslen, characters) + "\n";
+            }
+        }
+        return(output);
+    }
+
+    //----- End Joe Emenaker
+
     //----- Start Hiroo Hayashi
     //
     // convert MidiMessage or byte array into String
@@ -74,56 +205,29 @@ public class Utility extends Object {
      * @return hexa-dump string.
      */
     public static String hexDump(byte[] d, int offset, int len, int bytes) {
-	StringBuffer buf = new StringBuffer();
-	if (len == -1 || offset + len > d.length)
-	    len = d.length - offset;
-	for (int i = 0; i < len; i++) {
-	    int c = (d[offset + i] & 0xff);
-	    if (c < 0x10)
-		buf.append("0");
-	    buf.append(Integer.toHexString(c));
-	    if (bytes > 0
-		&& (i % bytes == bytes - 1 && i != len - 1))
-		buf.append("\n  ");
-	    else if (i != len - 1)
-		buf.append(" ");
-	}
-	return buf.toString();
+	    return hexDump(d,offset,len,bytes,true);
     }
 
     /**
-     * convert a byte array into a one-line hexa-dump string.
+     * convert a byte array into a one-line hexa-dump string. If it's longer than
+     * <code>len</code>, it will have the inner portion removed and replaced with
+     * dots, for example: "00 01 03 04 05 .. 7d 7e 7f"
      *
      * @param d a <code>byte[]</code> array to be converted.
      * @param offset array index from which dump starts.
      * @param len number of bytes to be dumped.  If -1, dumps to the
      * end of the array.
-     * @param bytes number of columns (bytes) per line.
+     * @param bytes number of columns (bytes) to put in the one-line string.
      * @return hexa-dump string.
      */
     public static String hexDumpOneLine(byte[] d, int offset, int len, int bytes) {
-	if (len == -1 || len > d.length - offset)
-	    len = d.length - offset;
+    	if (len == -1 || len > d.length - offset)
+	        len = d.length - offset;
 
-	if (len <= bytes || len < 8)
-	    return hexDump(d, offset, len, 0);
+    	if (len <= bytes || len < 8)
+	        return hexDump(d, offset, len, 0);
 
-	StringBuffer buf = new StringBuffer();
-	for (int i = 0; i < bytes - 4; i++) {
-	    int c = (d[offset + i] & 0xff);
-	    if (c < 0x10)
-		buf.append("0");
-	    buf.append(Integer.toHexString(c) + " ");
-	}
-	buf.append("..");
-	for (int i = len - 3; i < len; i++) {
-	    int c = (d[offset + i] & 0xff);
-	    buf.append(" ");
-	    if (c < 0x10)
-		buf.append("0");
-	    buf.append(Integer.toHexString(c));
-	}
-	return buf.toString();
+        return(hexDump(d,offset,bytes-4,0) + ".." + hexDump(d,offset + len - 3, 3, 0));
     }
     //----- End Hiroo Hayashi
 
