cb3b5d3799a017c4f977e21072a4ad771db2a96e
jemenake <jemenake@5361db82-7b37-4a53-9f17-e17e3a93d091>
2006-02-07 02:35:49 +0000

    Changed TextArea to JTextArea and fixed scrollpane

--- JSynthLib/synthdrivers/Generic/SingleTextAreaFrame.java
+++ JSynthLib/synthdrivers/Generic/SingleTextAreaFrame.java
@@ -3,6 +3,7 @@ package synthdrivers.Generic;
 import core.Patch;
 import core.PatchEditorFrame;
 
+import javax.swing.*;
 import java.awt.*;
 
 /**
@@ -13,9 +14,9 @@ import java.awt.*;
  */
 public class SingleTextAreaFrame extends PatchEditorFrame {
 
-    Font f = new Font("Courier", Font.PLAIN, 12);
-    int cols = 80;
-    private TextArea textArea;
+    Font monospaced = new Font("Courier", Font.PLAIN, 12);
+    int rows=20, cols = 80;
+    private JTextArea textArea;
 
     public SingleTextAreaFrame(String title, int cols) {
         this(title);
@@ -25,20 +26,24 @@ public class SingleTextAreaFrame extends PatchEditorFrame {
     SingleTextAreaFrame(String title) {
         super(title,new Patch(new byte[] {(byte) 0xF0, (byte) 0xF7}));
 
-        textArea = new TextArea();
-        textArea.setFont(f);
+        textArea = new JTextArea(rows, cols);
         textArea.setEditable(false);
+        textArea.setFont(monospaced);
 
         // Get the width of a single character (we'd better have a fixed-width font) and use it
         // to calculate the needed pixel width of the textArea. Then, resize...
-        int charwidth = textArea.getFontMetrics(f).stringWidth(" ");
-        int pixelwidth = cols * charwidth;
-        textArea.setPreferredSize(new Dimension(pixelwidth + 50,200));
+//        int charwidth = textArea.getFontMetrics(f).stringWidth(" ");
+//        int pixelwidth = cols * charwidth;
+//        textArea.setPreferredSize(new Dimension(pixelwidth + 50,200));
 
-        getContentPane().setLayout(new BorderLayout());
-        getContentPane().add(textArea,java.awt.BorderLayout.CENTER);
-//        scrollPane.setLayout(new BorderLayout());
-//        scrollPane.add(textArea,java.awt.BorderLayout.CENTER);
+        JScrollPane sp = new JScrollPane(textArea);
+        sp.setBorder(BorderFactory.createEmptyBorder(10,5,10,5));
+        this.getContentPane().setLayout(new BorderLayout());
+        this.getContentPane().add(sp,BorderLayout.CENTER);
+
+        this.pack();
+        this.setVisible(true);
+
     }
 
     public void append(String str) {
